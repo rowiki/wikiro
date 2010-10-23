9d0dacc52faaac84939ac12c801904eb69f564d6
andrei.stroe <andrei.stroe@04516ab2-3ca3-11de-be05-5f7a86268029>
2010-10-23 09:28:53 +0000

    support for new symbol in commons filename

--- robots/python/pywikipedia/deletecommonsfiles.py
+++ robots/python/pywikipedia/deletecommonsfiles.py
@@ -13,7 +13,7 @@ imgsterse = 0
 for page in gen:
     if page.isImage():
 	text = page.get()
-	ex = re.compile(u"\{\{NowCommons(\|([\w\:\-\_\.\,\(\)\&\'\ żóéáìüäăşţőâĂŞȘŢȚÂșșțáöéüíПиднторубльаверс]*))?")
+	ex = re.compile(u"\{\{NowCommons(\|([\w\:\-\_\.\,\(\)\&\'\ żóéáìüäăşţőâÎĂŞȘŢȚÂșșțáöéüíПиднторубльаверс]*))?")
 	res = re.findall(ex, text)
  	
  	wikipedia.output(page.title())
