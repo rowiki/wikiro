fedb950789658fc5d20760eb24222807b80c25e1
strainu10@gmail.com <strainu10@gmail.com@04516ab2-3ca3-11de-be05-5f7a86268029>
2011-12-07 20:37:03 +0000

    Fully functional version of osm2wiki_template.py

--- robots/python/geo/osm2wiki_template.py
+++ robots/python/geo/osm2wiki_template.py
@@ -7,19 +7,17 @@ from a given area in OSM, then add a {{coord}} template to that Wikipedia articl
 
 The following command line parameters are supported:
 
--bbox               The are in which to search for elementsin the format longmin,latmin,longmax,latmax
-
--summary            Define the summary to use
+-start				Define the starting page in URL format
 
 -lang               Replace the language you are working on with another language in searching for wiki links
 
 --- Example ---
 1.
-# This is a script to add a {{coord}} template to the top of the 
-# pages with a wikipedia= or wikipedia:ro= entry in OSM in Romania
-# Warning! Put it in one line, otherwise it won't work correctly.
+# This is a script to add a the coordinates of the 
+# Romanian villages from OSM to the infobox on the
+# Wikipedia page
 
-python osm2wiki_template.py -bbox:20.2148438,43.7710938,29.7729492,48.1147666 -summary:"Bot: Adding a coordinate" -lang:ro
+python osm2wiki_template.py -summary:"Bot: Adding a coordinate" -lang:ro -start:Albac,_Alba
 
 
 """
@@ -73,6 +71,8 @@ class o2wVillageData:
 		self._osmFilename = "osmcoord2wiki.csv"
 		self._log = "villageData.log"
 		self._comment = "Adding link to wikipedia for village %s"
+		self._dict = {}
+		self._keyList = []
 		
 	def log(self, header, string):
 		f = open(self._log, 'a+')
@@ -91,64 +91,52 @@ class o2wVillageData:
 	def logw(self, string):
 		self.log("* WIKI error (%s): " % time.strftime("%Y-%m-%d %H:%M:%S"), repr(string))
 		
-	#this is future code
-	def tl2Dict(self, template):
-		template = re.sub(r'\s', '', template)
-		dic = {}
-		params = template.split('|')
-		for line in params:
-			line = line.split('=')
-			if len(line) is 2:
-				dic[line[0]] = line[1]
-				#TODO:this is bullshit
-			else:
-				continue
-		print dic
-		return dic
+	def logd(self, string):
+		self.log("* Debug (%s): " % time.strftime("%Y-%m-%d %H:%M:%S"), repr(string))
 		
-	def getTemplateParam(self, template, param, value, text):
-		if template == None:
+	#extract the first instance of a template 
+	#from the page text
+	def extractTemplate(self, text, template):
+		match = re.search("\{\{" + template, text)
+		if match == None:
 			return None
-		# dirty hack: the first regexp blocks when not finding a match, so we simply strip the whitespace
-		# from the template and do the matching with a simpler regexp
-		#regexp = "(\{\{" + template + "(.|\r|\n)*?" + param + "([ ]*?)=([ ]*?)" + value + ")" #(.|\r|\n)*?\}\})"
-		template = re.sub(r'\s', '', template)
-		regexp = "(\{\{" + template + ".*?" + param + "=" + value + ")"
-		regexp = regexp.decode("utf8")
-		#pywikibot.output(regexp)
-		text = re.sub(r'\s', '', text)
-		#pywikibot.output(text)
-		template_regexp  = re.compile(regexp, re.I | re.S);
-		template_match = template_regexp.search(text)
-		
-		if template_match <> None and template_match.lastindex <> None:
-			pywikibot.output(template_match.group(3))
-			return template_match.group(3) #value is the third regexp
-		else:
-			return None
+		tl = text[match.start():]
+		text = text[match.start() + 2:]
+		open = 0
+		close = 1
+		while open < close:
+			open = text.find("{{")
+			close = text.find("}}")
+			text = text[close + 2:]
+		tl = tl.replace(text, "")
+		self.logd("I have extracted template: " + tl)
+		return tl
 	
-	def setValueToTemplate(self, text, template, param, value, newValue, exists):
-		if exists:
-			regexp = "(\{\{" + template.decode("utf8") + ".*?(" + param.decode("utf8") + "\s*=\s*" + str(value) + "))"
-			pywikibot.output(regexp)
-			template_regexp  = re.compile(regexp, re.I | re.S);
-			template_match = template_regexp.search(text)
-			if template_match <> None and template_match.lastindex <> None:
-				#pywikibot.output("ala" + str(template_match.groups()))
-				#key=value is the third regexp
-				newKeyValue = param.decode("utf8") + "=" + str(newValue)
-				text = text.replace(template_match.group(3), newKeyValue)
-				self.logi("Changed " + template_match.group(3) + " with " + newKeyValue); 
-		else:
-			print "Field " + param + " does not exits"
-			template_regexp  = re.compile(template.decode("utf8"), re.I | re.S);
-			template_match = template_regexp.search(text)
-			newKeyValue = template_match.group(0) + "\n|" + param.decode("utf8") + "=" + str(newValue)
-			text = text.replace(template_match.group(0), newKeyValue)
-			self.logi("Added " + newKeyValue + " to the template")
-			#TODO: put the parameter in the right place
-			pass
-		return text
+	#convert a template to a dictionary
+	#since we're using python2, the dictionary
+	#class is unordered. We therefore need to 
+	#keep the order of the keys in order to be
+	#able to reconstruct the tmeplate
+	def tl2Dict(self, template):
+		template = re.sub(r'(\r|\n)', '', template)
+		template = template[0:len(template)-2]#get rid of '}}'
+		params = template.split('|')
+		key = ""
+		for line in params:
+			#print line
+			line = line.split('=')
+			if (len(line) > 1):
+				key = line[0].encode("utf8")
+				key = re.sub(r'\s', '', key)
+				value = "=".join(line[1:]).encode("utf8")
+				self._dict[key] = str(value)
+				self._keyList.append(key)
+			elif line[0].startswith('{{'): #name of the template
+				self._dict["_name"] = line[0][2:]
+				self._keyList.append("_name")
+			elif line[0] != "" and key != "":#the first line might not begin with {{
+				self._dict[key] = self._dict[key] + "|" + line[0].encode("utf8")
+		return self._dict
 		
 	def getDeg(self, decimal):
 		if decimal < 0:
@@ -172,6 +160,32 @@ class o2wVillageData:
 			return -1
 		else:
 			return 0 #this should really never happen
+			
+	def insertKeyInKeyList(self, key):
+		index = len(self._keyList)
+		try:
+			if key == "latd":
+				index = 1 + self._keyList.index("codpoștal")
+				 #if it doesn't exist, the default value will be used
+			elif key == "latm":
+				index = 1 + self._keyList.index("latd")
+			elif key == "lats":
+				index = 1 + self._keyList.index("latm")
+			elif key == "latNS":
+				index = 1 + self._keyList.index("lats")
+			elif key == "longd":
+				index = 1 + self._keyList.index("latNS")
+			elif key == "longm":
+				index = 1 + self._keyList.index("longd")
+			elif key == "longs":
+				index = 1 + self._keyList.index("longm")
+			elif key == "longEV":
+				index = 1 + self._keyList.index("longs")
+			elif key == "codpoștal":
+				index = 1 + self._keyList.index("longEV")
+		except:
+			pass #we're ok with the default value
+		self._keyList.insert(index, key)
 
 	def putCoordOnWiki(self, lang, firstArticle):
 		if firstArticle == None:
@@ -180,27 +194,30 @@ class o2wVillageData:
 			start = 0
 		site = pywikibot.getSite(lang)
 		reader = csv.reader(open(self._osmFilename, "r"), delimiter='\t')
-		village_templates = "(CutieSate|CasetăSate|Infocaseta Așezare|Infobox aşezare|Casetă așezare|Cutie așezare)"
+		village_templates = "(CutieSate|CasetăSate|Infocaseta Așezare|Infobox aşezare|Casetă așezare|Cutie așezare)".decode('utf8')
 		for row in reader:
+			self._keyList = []
+			self._dict = {}
 			#row=[title, urlname, latitude, longitude, coord_need_update, postal_code, nodeid, Truelat, Truelong, Truecode]
-			title = row[1]				
+			title = row[0]				
+			wikiTitle = row[1]
 			
-			if firstArticle == title:
+			if firstArticle == wikiTitle:
 				start = 1
 			if start == 0:
 				continue
 				
-			osmCode = row[5]
 			osmLatd = self.getDeg(float(row[7]))
 			osmLatm = self.getMin(float(row[7]))
 			osmLats = self.getSec(float(row[7]))
 			osmLongd = self.getDeg(float(row[8]))
 			osmLongm = self.getMin(float(row[8]))
 			osmLongs = self.getSec(float(row[8]))
-			pywikibot.output("\n")
-			self.logi("Parsing page: %s" % row[0])
-			self.logi("Coords: %d %d %d lat N %d %d %d long E" % (osmLatd, osmLatm, osmLats, osmLongd, osmLongm, osmLongs))
-			generator = pywikibot.Page(site, title)
+			osmCode = row[9]
+			
+			self.logi("Parsing page: %s" % title)
+			self.logi("Coords: %d %d %d lat N %d %d %d long E, code %s" % (osmLatd, osmLatm, osmLats, osmLongd, osmLongm, osmLongs, osmCode))
+			generator = pywikibot.Page(site, wikiTitle)
 			if generator.isRedirectPage():
 				generator = generator.getRedirectTarget()
 			try:
@@ -208,115 +225,145 @@ class o2wVillageData:
 			except Exception as inst:
 				self.loge(u"Unknown error in getTagValue, exiting: %s" % inst);
 				
-			code = self.getTemplateParam(village_templates, "codpoștal", "([0-9]{5,6})", text)
-			if code <> None:
-				if code <> osmCode:
-					text = self.setValueToTemplate(text, village_templates, "codpoștal", code, osmCode, True)
-				else:
-					self.logi("The postal code field is already correct");
-			else:
-				text = self.setValueToTemplate(text, village_templates, "codpoștal", code, osmCode, False)
+			oldTl = self.extractTemplate(text, village_templates)
+			if oldTl == None:
+				self.loge("No template in page %s" % title)
+				continue
+			self.tl2Dict(oldTl)#populate self._dict
+			print self._dict
 				
-			latd = self.getTemplateParam(village_templates, "latd", "([0-9\.]+)", text)
-			if latd <> None:
+			try:
+				latd = self._dict["latd"]
 				latd = latd.replace(",", ".") #make sure we're dealing with US-style numbers
 				latd = int(latd)
 				if latd <> osmLatd:
-					text = self.setValueToTemplate(text, village_templates, "latd", latd, osmLatd, True)
+					self._dict["latd"] = str(osmLatd)
 				else:
 					self.logi("The latitude degrees field is already correct");
-			else:
-				text = self.setValueToTemplate(text, village_templates, "latd", latd, osmLatd, False)
+			except:
+				self._dict["latd"] = str(osmLatd)
+				self.insertKeyInKeyList("latd")
 				
-			latm = self.getTemplateParam(village_templates, "latm", "([0-9\.]+)", text)
-			if latm <> None:
+			try:
+				latm = self._dict["latm"]
 				latm = latm.replace(",", ".") #make sure we're dealing with US-style numbers
 				latm = int(latm)
 				if latm <> osmLatm:
-					text = self.setValueToTemplate(text, village_templates, "latm", latm, osmLatm, True)
+					self._dict["latm"] = str(osmLatm)
 				else:
 					self.logi("The latitude minutes field is already correct");
-			else:
-				text = self.setValueToTemplate(text, village_templates, "latm", latm, osmLatm, False)
+			except:
+				self._dict["latm"] = str(osmLatm)
+				self.insertKeyInKeyList("latm")
 				
-			lats = self.getTemplateParam(village_templates, "lats", "([0-9\.]+)", text)
-			if lats <> None:
+			try:
+				lats = self._dict["lats"]
 				lats = lats.replace(",", ".") #make sure we're dealing with US-style numbers
 				lats = int(lats)
 				if lats<> osmLats:
-					text = self.setValueToTemplate(text, village_templates, "lats", lats, osmLats, True)
+					self._dict["lats"] = str(osmLats)
 				else:
 					self.logi("The latitude seconds field is already correct");
-			else:
-				text = self.setValueToTemplate(text, village_templates, "lats", lats, osmLats, False)
+			except:
+				self._dict["lats"] = str(osmLats)
+				self.insertKeyInKeyList("lats")
 				
-			latNS = self.getTemplateParam(village_templates, "latNS", "(N|S)", text)
-			if latNS == None:
-				text = self.setValueToTemplate(text, village_templates, "latNS", latNS, "N", False)
-			else:
+			try:
+				latNS = self._dict["latNS"]
 				self.logi("The latNS field already exists")
+			except:
+				self._dict["latNS"] = "N"
+				self.insertKeyInKeyList("latNS")
 				
-			longd = self.getTemplateParam(village_templates, "longd", "([0-9\.]+)", text)
-			if longd <> None:
+			try:
+				longd = self._dict["longd"]
 				longd = longd.replace(",", ".") #make sure we're dealing with US-style numbers
 				longd = int(longd)
 				if longd <> osmLongd:
-					text = self.setValueToTemplate(text, village_templates, "longd", longd, osmLongd, True)
+					self._dict["longd"] = str(osmLongd)
 				else:
 					self.logi("The longitude degrees field is already correct");
-			else:
-				text = self.setValueToTemplate(text, village_templates, "longd", longd, osmLongd, False)
+			except:
+				self._dict["longd"] = str(osmLongd)
+				self.insertKeyInKeyList("longd")
 				
-			longm = self.getTemplateParam(village_templates, "longm", "([0-9\.]+)", text)
-			if longm <> None:
+			try:
+				longm = self._dict["longm"]
 				longm = longm.replace(",", ".") #make sure we're dealing with US-style numbers
 				longm = int(longm)
 				if longm <> osmLongm:
-					text = self.setValueToTemplate(text, village_templates, "longm", longm, osmLongm, True)
+					self._dict["longm"] = str(osmLongm)
 				else:
 					self.logi("The longitude minutes field is already correct");
-			else:
-				text = self.setValueToTemplate(text, village_templates, "longm", longm, osmLongm, False)
+			except:
+				self._dict["longm"] = str(osmLongm)
+				self.insertKeyInKeyList("longm")
 				
-			longs = self.getTemplateParam(village_templates, "longs", "([0-9\.]+)", text)
-			if longs <> None:
+			try:
+				longs = self._dict["longs"]
 				longs = longs.replace(",", ".") #make sure we're dealing with US-style numbers
 				longs = int(longs)
 				if longs <> osmLongs:
-					text = self.setValueToTemplate(text, village_templates, "longs", longs, osmLongs, True)
+					self._dict["longs"] = str(osmLongs)
 				else:
 					self.logi("The longitude seconds field is already correct");
-			else:
-				text = self.setValueToTemplate(text, village_templates, "longs", longs, osmLongs, False)
+			except:
+				self._dict["longs"] = str(osmLongs)
+				self.insertKeyInKeyList("longs")
 				
-			longEV = self.getTemplateParam(village_templates, "longEV", "(E|V)", text)
-			if longEV == None:
-				text = self.setValueToTemplate(text, village_templates, "latEV", longEV, "E", False)
-			else:
+			try:
+				longEV = self._dict["longEV"]
 				self.logi("The longEV field already exists")
-						
-			#TODO: remove "coordonate" field
-			#generator.put(text)
+			except:
+				self._dict["longEV"] = "E"
+				self.insertKeyInKeyList("longEV")
+				
+			try:
+				code = self._dict["codpoștal"]
+				print code
+				print osmCode
+				if code <> osmCode:
+					self._dict["codpoștal"] = str(osmCode)
+				else:
+					self.logi("The postal code field is already correct");
+			except:
+				self._dict["codpoștal"] = str(osmCode)
+				self.insertKeyInKeyList("codpoștal")
+				
+			try:
+				#the same key might be present more than
+				#once in the list, but not the dictionary
+				while "coordonate" in self._keyList:
+					self._keyList.remove("coordonate")
+				del self._dict["coordonate"]
+			except:
+				self.logi("Problems deleting coordonate key");
+			
+			tl = u""
+			for key in self._keyList:
+				if key == "_name":
+					tl += "{{" + self._dict[key] + "\n"
+				else:
+					tl += "| " + key.decode("utf8") + " = " 
+					tl += self._dict[key].decode("utf8") + "\n"
+			tl += "}}"
+			print self._dict		
+			self.logd("New template: " + tl)
+			# we compare the 2 templates without whitespace 
+			# to determine if we made any replacement
+			if cmp(re.sub(r'\s', '', oldTl), re.sub(r'\s', '', tl)):
+				text = text.replace(oldTl, tl)
+				comment = pywikibot.translate(site, msg) % title.decode("utf8")
+				generator.put(text, comment)
+			else:
+				self.logi("No change for current page")
 		
 def main():
-	bbox="20.2148438,43.7710938,29.7729492,48.1147666" #default to Romania
-	summary=None
 	lang=pywikibot.getSite().language()
 	start = None
 	# Loading the arguments
 	for arg in pywikibot.handleArgs():
-		if arg.startswith('-bbox'):
-			if len(arg) == 5:
-				bbox = pywikibot.input(
-					u'Please input the area to search for tagged nodes:')
-			else:
-				bbox = arg[6:]
-		elif arg.startswith('-summary'):
-			if len(arg) == 8:
-				summary = pywikibot.input(u'What summary do you want to use?')
-			else:
-				summary = arg[9:]
-		elif arg.startswith('-lang'):
+		if arg.startswith('-lang'):
 			if len(arg) == 5:
 				lang = pywikibot.input(u'What language do you want to use?')
 			else:
@@ -326,8 +373,6 @@ def main():
 				start = pywikibot.input(u'What article do you want to start with?')
 			else:
 				start = arg[7:]
-		
-	#pages = getPageList(bbox, lang)
 	
 	bot = o2wVillageData()
 	bot.putCoordOnWiki(lang, start)
