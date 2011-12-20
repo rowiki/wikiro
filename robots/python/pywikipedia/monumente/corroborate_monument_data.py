0c879dd3e53550c59703e6524f56b9dd39f346a5
strainu10@gmail.com <strainu10@gmail.com@04516ab2-3ca3-11de-be05-5f7a86268029>
2011-12-20 17:32:59 +0000

    Add monument parsing robots.

---
+++ robots/python/pywikipedia/monumente/corroborate_monument_data.py
@@ -0,0 +1,148 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8  -*-
+'''
+Based on the output from parse_monument_article.py and update_database.py 
+identify different improvements and further errors in the monument pages and database
+* să adauge linkuri către articolul despre un monument acolo unde există - d
+* să adauge coordonate în listă dacă ele există în articol - d
+* să adauge automat poza în listă dacă există una singură cu monumentul respectiv - d
+* să raporteze la [[Proiect:Monumente istorice/Erori/Automate]] următoarele situații:
+** în listă nu există codul respectiv, dar codul are formatul corect în articol/imagine
+** în listă nu există imagine și există mai multe imagini disponibile la commons - i
+** în listă și articol coordonatele sunt diferite (diferențe mai mari de 0,01 grade sau ~35 de secunde de grad) - i
+'''
+
+import sys, time, warnings, json, string
+sys.path.append("..")
+import wikipedia, re, pagegenerators
+import config as user
+import math
+
+_log = "link.err.log"
+_flog = None
+
+def initLog():
+	global _flog, _log;
+	_flog = open(_log, 'w+')
+	
+def closeLog():
+	global _flog
+	_flog.close()
+
+def log(string):
+	_flog.write(string.encode("utf8") + "\n")
+	
+def main():
+	f = open("db.json", "r+")
+	wikipedia.output("Reading database file...")
+	db = json.load(f)
+	wikipedia.output("...done")
+	f.close();
+	
+	f = open("ro_pages.json", "r+")
+	wikipedia.output("Reading ro.wp pages file...")
+	pages_ro = json.load(f)
+	wikipedia.output("...done")
+	f.close();
+	
+	f = open("commons_pages.json", "r+")
+	wikipedia.output("Reading commons pages file...")
+	pages_commons = json.load(f)
+	wikipedia.output("...done")
+	f.close();
+	
+	initLog()
+	#this is the big loop that should only happen once
+	for monument in db:
+		code = monument["Cod"]
+		regexp = re.compile("(([a-z]{1,2})-(i|ii|iii|iv)-([a-z])-([a-z])-([0-9]{5}(\.[0-9]{2})?))", re.I)
+		result = re.findall(regexp, code)
+		if len(result) > 0:
+			code = result[0][0]
+		wikipedia.output(code)
+		allPages = list()
+		article = None
+		picture = None
+		#wikipedia.output(str(page))
+		try:
+			#wikipedia.output("OK: " + str(page[code]))
+			if code in pages_ro:
+				allPages.extend(pages_ro[code])
+				if len(allPages) > 1:
+					msg = u"*''E'': Codul ''%s'' este prezent în mai multe articole pe Wikipedia: " % code
+					for page in allPages:
+						msg += (u"[[:%s]] " % page["name"])
+					msg += u"\n"
+					log(msg)
+				elif len(allPages) == 1:
+					article = allPages[0]["name"]
+			if code in pages_commons:
+				if len(pages_commons[code]) == 1: #exactly one picture
+					picture = pages_commons[code][0]["name"]
+				elif monument["Imagine"] == "": #no image, multiple available
+					msg = u"*''I'': Există mai multe imagini disponibile la commons pentru codul ''%s'': " % code
+					for pic in pages_commons[code]:
+						msg += u"[[:File:%s]], " % pic["name"]
+					msg += "\n"
+					log(msg)
+				allPages.extend(pages_commons[code])
+			#wikipedia.output(str(allPages))
+		except Exception as e:
+			wikipedia.output("Error: " + str(e))
+			pass #ignore errors
+			
+		if article <> None:
+			wikipedia.output(article)
+			pass
+			#TODO put a link in the list
+			
+		if picture <> None and monument["Imagine"] <> "":
+			wikipedia.output(picture)
+			pass
+			#TODO put an image in the list
+		
+		if monument["Lat"] == "":
+			lat = 0
+		else:
+			lat = float(monument["Lat"])
+		if monument["Lon"] == "":
+			long = 0
+		else:
+			long = float(monument["Lon"])
+		artLat = 0
+		artLong = 0
+		artCoord = None
+		updateCoord = True
+		for page in allPages:
+			if page["lat"] <> 0 and page["long"] <> 0:
+				if artLat <> 0: #this also means artLong <> 0
+					if math.fabs(artLat - page["lat"]) > 0.01 or math.fabs(artLong - page["long"]) > 0.01:
+						if page["namespace"] == 6:
+							namespace = "File:"
+						else:
+							namespace = ""
+						log(u"* ''E:'': Coordonate diferite pentru codul %s între [[:%s%s]] (%f,%f) și [[:%s]] (%f,%f)" % (code, namespace, page["name"], page["lat"], page["long"], artCoord, artLat, artLong))
+						updateCoord = False
+				else:
+					artLat = page["lat"]
+					artLong = page["long"]
+					if page["namespace"] == 6:
+						artCoord = "File:" + page["name"]
+					else:
+						artCoord = page["name"]
+					
+		if lat == 0 and artLat <> 0 and updateCoord:
+			wikipedia.output(str(artLat) + " " + str(artLong))
+			pass
+			#TODO: update coords on page
+		
+		if lat <> 0 and artLat <> 0 and (math.fabs(artLat - lat) > 0.01 or math.fabs(artLong - long) > 0.01):
+			log(u"* ''E:'': Coordonate diferite pentru codul %s între [[:%s]] (%f,%f) și listă (%f,%f)" % (code, artCoord, artLat, artLong, lat, long))
+		
+	closeLog()
+
+if __name__ == "__main__":
+	try:
+		main()
+	finally:
+		wikipedia.stopme()
---
+++ robots/python/pywikipedia/monumente/update_database.py
@@ -0,0 +1,215 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8  -*-
+'''
+Update the monuments database either from a text file or from some wiki page(s)
+
+'''
+import sys, time, warnings, json
+sys.path.append("..")
+import wikipedia, config, re, pagegenerators
+
+countries = {
+	('ro', 'ro') : {
+		'project' : u'wikipedia',
+	'lang' : u'ro',
+	'headerTemplate' : u'ÎnceputTabelLMI',
+	'rowTemplate' : u'ElementLMI',
+		'footerTemplate' : u'SfârșitTabelLMI',
+		'commonsTemplate' : u'Monument istoric',
+		'commonsTrackerCategory' : u'Cultural heritage monuments in Romania with known IDs',
+		'commonsCategoryBase' : u'Historical monuments in Romania',
+		'unusedImagesPage' : u'User:Multichill/Unused Monument istoric',
+		'imagesWithoutIdPage' : u'User:Multichill/Monument istoric without ID',
+	'namespaces' : [0],
+	'table' : u'monuments_ro_(ro)',
+	'truncate' : False, 
+	'primkey' : u'Cod',
+	'fields' : [
+		{
+		'source' : u'Cod',
+		'dest' : u'cod',
+		'conv' : u'',
+		},
+		{
+		'source' : u'Denumire',
+		'dest' : u'denumire',
+		'conv' : u'',
+		},
+		{
+		'source' : u'Localitate',
+		'dest' : u'localitate',
+		'conv' : u'',
+		},
+			{
+		'source' : u'Adresă',
+		'dest' : u'adresa',
+		'conv' : u'',
+		},
+		{
+		'source' : u'Datare',
+		'dest' : u'datare',
+		'conv' : u'',
+		},
+		{
+		'source' : u'Arhitect',
+		'dest' : u'arhitect',
+		'conv' : u'',
+		},
+		{
+		'source' : u'Lat',
+		'dest' : u'lat',
+		'conv' : u'',
+		},
+		{
+		'source' : u'Coordonate',
+		'dest' : u'',
+		'conv' : u'',
+		},
+		{
+		'source' : u'Lon',
+		'dest' : u'lon',
+		'conv' : u'',
+		},
+		{
+		'source' : u'Imagine',
+		'dest' : u'imagine',
+		'conv' : u'',
+		},
+		],
+	},
+}
+
+monuments_db = []
+
+def processMonument(params, source, countryconfig, title):
+	'''
+	Process a single instance of a monument row template
+	'''
+	 
+	# Get all the fields
+	contents = {}
+	# Add the source of information (permalink)
+	contents['source'] = source
+	for field in countryconfig.get('fields'):
+		contents[field.get(u'source')]=u''
+
+	for param in params:
+		#Split at =
+		(field, sep, value) = param.partition(u'=')
+		# Remove leading or trailing spaces
+		field = field.strip()
+		value = value.split("<ref")[0].strip()
+		
+		#Check first that field is not empty
+		if field:
+				#Is it in the fields list?
+				if field in contents:
+					#Load it with Big fucking escape hack. Stupid mysql lib
+					if field == "Cod":
+						contents[field] = re.sub(r'\s', '', value.encode("utf8")) # Do this somewhere else.replace("'", "\\'")
+					else:
+						contents[field] = value.encode("utf8") # Do this somewhere else.replace("'", "\\'")
+				else:
+					#FIXME: Include more information where it went wrong
+					wikipedia.output(u'Found unknown field: %s on page %s' % (field, title) )
+					wikipedia.output(u'Field: %s' % (field,))
+					wikipedia.output(u'Value: %s' % (value,))
+					#time.sleep(5)
+	return contents
+
+def processText(text, source, countryconfig, page=None):
+	'''
+	Process a text containing one or multiple instances of the monument row template
+	'''
+	
+	if not page:
+		site = site = wikipedia.getSite(countryconfig.get('lang'), countryconfig.get('project'))
+		page = wikipedia.Page(site, u'User:Multichill/Zandbak')
+	templates = page.templatesWithParams(thistxt=text)
+	wikipedia.output(u'Working on page "%s"' % page.title(True))
+	for (template, params) in templates:
+		if template==countryconfig.get('rowTemplate'):
+			#print template
+			#print params
+			monuments_db.append(processMonument(params, source, countryconfig, page.title(True)))
+			#time.sleep(5)
+ 
+
+def processCountry(countryconfig):
+	'''
+	Process all the monuments of one country
+	'''
+
+	site = wikipedia.getSite(countryconfig.get('lang'), countryconfig.get('project'))
+	rowTemplate = wikipedia.Page(site, u'%s:%s' % (site.namespace(10), countryconfig.get('rowTemplate')))
+
+	transGen = pagegenerators.ReferringPageGenerator(rowTemplate, onlyTemplateInclusion=True)
+	filteredGen = pagegenerators.NamespaceFilterPageGenerator(transGen, countryconfig.get('namespaces'))
+	pregenerator = pagegenerators.PreloadingGenerator(filteredGen)
+	for page in pregenerator:
+		if page.exists() and not page.isRedirectPage():
+			# Do some checking
+			processText(page.get(), page.permalink(), countryconfig, page=page)
+			
+	f = open("db.json", "w+")
+	json.dump(monuments_db, f)
+	f.close();
+
+
+def processTextfile(textfile, countryconfig,):
+	'''
+	Process the contents of a text file containing one or more lines with the Tabelrij rijksmonument template
+	'''
+	file = open(textfile, 'r')
+	for line in file:
+		processText(line.decode('UTF-8').strip(), textfile, countryconfig)
+
+def main():
+	'''
+	The main loop
+	'''
+	# First find out what to work on
+
+	countrycode = u'ro'
+	textfile = u''
+
+	for arg in wikipedia.handleArgs():
+		if arg.startswith('-countrycode:'):
+			countrycode = arg [len('-countrycode:'):]
+		elif arg.startswith('-textfile:'):
+			textfile = arg [len('-textfile:'):]
+
+	if countrycode:
+		lang = wikipedia.getSite().language()
+		if not countries.get((countrycode, lang)):
+			wikipedia.output(u'I have no config for countrycode "%s" in language "%s"' % (countrycode, lang))
+			return False
+		wikipedia.output(u'Working on countrycode "%s" in language "%s"' % (countrycode, lang))
+		if textfile:
+			wikipedia.output(u'Going to work on textfile.')
+			processTextfile(textfile, countries.get((countrycode, lang)))
+		else:
+			processCountry(countries.get((countrycode, lang)))
+	else:
+		for (countrycode, lang), countryconfig in countries.iteritems():
+			wikipedia.output(u'Working on countrycode "%s" in language "%s"' % (countrycode, lang))
+			processCountry(countryconfig,)
+	'''
+
+
+	generator = genFactory.getCombinedGenerator()
+	if not generator:
+		wikipedia.output(u'You have to specify what to work on. This can either be -textfile:<filename> to work on a local file or you can use one of the standard pagegenerators (in pagegenerators.py)')
+	else:
+		pregenerator = pagegenerators.PreloadingGenerator(generator)
+		for page in pregenerator:
+		if page.exists() and not page.isRedirectPage():
+			# Do some checking
+			processText(page.get(), page.permalink(), page=page)
+	'''
+
+if __name__ == "__main__":
+	try:
+		main()
+	finally:
+		wikipedia.stopme()
