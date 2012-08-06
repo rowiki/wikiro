bce26127be8d6ee2de7251981608806afcd80f5c
andrei.stroe <andrei.stroe@04516ab2-3ca3-11de-be05-5f7a86268029>
2012-08-06 15:14:39 +0000

    WikiAutoSigner - begun parsing diffs; almost finished adding signature for new pages; committing pom.xml and new structure.

--- trunk/robots/java/WikiAutoSigner/src/main/java/com/googlecode/wikiro/was/DiffParser.java
+++ trunk/robots/java/WikiAutoSigner/src/main/java/com/googlecode/wikiro/was/DiffParser.java
@@ -1,16 +1,34 @@
 package com.googlecode.wikiro.was;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.wikipedia.Wiki.Revision;
+
+import difflib.Delta;
+import difflib.DiffUtils;
+import difflib.Patch;
 
 public class DiffParser {
-    private String diff;
+    private Revision prevRevision;
+    private Revision crtRevision;
     private String addedText;
     private String removedText;
 
-    public DiffParser(String diff) {
+    public DiffParser(Revision prevRevision, Revision crtRevision) {
         super();
-        this.diff = diff;
+        this.prevRevision = prevRevision;
+        this.crtRevision = crtRevision;
     }
-    
-    public void analyze() {
+
+    public void analyze() throws IOException {
+        List<String> prevContents = Arrays.asList(prevRevision.getText().split("\\r?\\n"));
+        List<String> crtContents = Arrays.asList(crtRevision.getText().split("\\r?\\n"));
+        
+        Patch diff = DiffUtils.diff(prevContents, crtContents);
+        for (Delta delta : diff.getDeltas()) {
+            System.out.println(delta);
+        }
     }
 }
