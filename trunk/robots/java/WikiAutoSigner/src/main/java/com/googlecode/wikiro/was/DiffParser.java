c07d379f50a48ffc2704bc43d7298293944c17b7
andrei.stroe <andrei.stroe@04516ab2-3ca3-11de-be05-5f7a86268029>
2012-08-06 13:40:36 +0000

    Refactoring to use Maven to manage deps

---
+++ trunk/robots/java/WikiAutoSigner/src/main/java/com/googlecode/wikiro/was/DiffParser.java
@@ -0,0 +1,16 @@
+package com.googlecode.wikiro.was;
+
+
+public class DiffParser {
+    private String diff;
+    private String addedText;
+    private String removedText;
+
+    public DiffParser(String diff) {
+        super();
+        this.diff = diff;
+    }
+    
+    public void analyze() {
+    }
+}
