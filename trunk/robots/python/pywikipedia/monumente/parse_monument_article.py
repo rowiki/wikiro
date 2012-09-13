f2e78768c99494cc4c86b0a3b107f06787146009
strainu10@gmail.com <strainu10@gmail.com@04516ab2-3ca3-11de-be05-5f7a86268029>
2012-09-13 06:27:34 +0000

    Implement an incremental system for parsing pages with LMI codes.
    The old system did not scale for the 18k+ files on commons. See the data directory for an initial json file.

--- trunk/robots/python/pywikipedia/monumente/parse_monument_article.py
+++ trunk/robots/python/pywikipedia/monumente/parse_monument_article.py
@@ -28,30 +28,30 @@ options = {
         'infoboxes':
         [
         {
-	        'name': u'Infocaseta Monument',
-	        'author': u'artist',
-	        'image': u'imagine',
+            'name': u'Infocaseta Monument',
+            'author': u'artist',
+            'image': u'imagine',
         },
         {
-	        'name': u'Clădire Istorică|Cutie Edificiu Religios|Infocaseta Edificiu religios|Infocaseta Monument|Infocaseta Lăcaș de cult',
-	        'author': u'arhitect',
-	        'image': u'imagine',
+            'name': u'Clădire Istorică|Cutie Edificiu Religios|Infocaseta Edificiu religios|Infocaseta Monument|Infocaseta Lăcaș de cult',
+            'author': u'arhitect',
+            'image': u'imagine',
         },
         {
-	        'name': u'Castru|Infocaseta Castru|Infocaseta Gară|Mănăstire',
-	        #'artist' does not really exist, putting it here to prevent errors
-	        'author': u'artist',
-	        'image': u'imagine',
+            'name': u'Castru|Infocaseta Castru|Infocaseta Gară|Mănăstire',
+            #'artist' does not really exist, putting it here to prevent errors
+            'author': u'artist',
+            'image': u'imagine',
         },
         {
-	        'name': u'Infocaseta Biserică din lemn',
-	        'author': u'meșteri',
-	        'image': u'imagine',
+            'name': u'Infocaseta Biserică din lemn',
+            'author': u'meșteri',
+            'image': u'imagine',
         },
         {
-	        'name': u'Infocaseta Lăcaș de cult',
-	        'author': u'constructor',
-	        'image': u'imagine',
+            'name': u'Infocaseta Lăcaș de cult',
+            'author': u'constructor',
+            'image': u'imagine',
         },
         ],
         'qualityTemplates':
@@ -95,6 +95,7 @@ def closeLog():
     _flog.close()
 
 def log(string):
+    //wikipedia.output(string.encode("utf8") + "\n")
     _flog.write(string.encode("utf8") + "\n")
 
 def dms2dec(deg, min, sec, sign):
@@ -112,7 +113,7 @@ def parseGeohackLinks(page):
     #title = page.title()
     #html = page.site().getUrl( "/wiki/" + page.urlname(), True)
     output = page.site().getUrl("/w/api.php?action=parse&format=json&page=" +
-	        page.urlname() + "&prop=externallinks&uselang=ro")
+            page.urlname() + "&prop=externallinks&uselang=ro")
     linksdb = json.loads(output)
     title = linksdb["parse"]["title"]
     links = linksdb["parse"]["externallinks"]
@@ -121,9 +122,9 @@ def parseGeohackLinks(page):
     for item in links:
         geohack_match = geohackRegexp.search(item)
         if geohack_match <> None:
-	        link = geohack_match.group(3)
-	        #print geohack_match.group(3)
-	        break
+            link = geohack_match.group(3)
+            #print geohack_match.group(3)
+            break
     if geohack_match == None or link == None or link == "":
         #wikipedia.output("No geohack link found in article")
         return 0,0
@@ -141,48 +142,48 @@ def parseGeohackLinks(page):
     l = tokens[:]
     for token in l:
         if token.strip() == '' or string.find(token, ':') > -1 or \
-		        string.find(token, '{{{') > -1:
-	        tokens.remove(token)
+                string.find(token, '{{{') > -1:
+            tokens.remove(token)
     numElem = len(tokens)
     if tokens[0] == link: #no _
         tokens = tokens[0].split(';')
         if float(tokens[0]) and float(tokens[1]): # D;D
-	        lat = tokens[0]
-	        long = tokens[1]
+            lat = tokens[0]
+            long = tokens[1]
         else:
-	        log(u"*''E'': [[:%s]] Problemă (1) cu legătura Geohack: nu pot \
-		        identifica coordonatele în grade zecimale: %s" % (title, link))
-	        return 0,0
+            log(u"*''E'': [[:%s]] Problemă (1) cu legătura Geohack: nu pot \
+                identifica coordonatele în grade zecimale: %s" % (title, link))
+            return 0,0
     elif numElem == 9: # D_N_D_E_to_D_N_D_E or D_M_S_N_D_M_S_E_something
         if tokens[4] <> "to":
-	        wikipedia.output(u"*[[%s]] We should ignore parameter 9: %s (%s)" %
-					        (title, tokens[8], link))
-	        deg1 = float(tokens[0])
-	        min1 = float(tokens[1])
-	        sec1 = float(tokens[2])
-	        sign1 = geosign(tokens[3],'N','S')
-	        deg2 = float(tokens[4])
-	        min2 = float(tokens[5])
-	        sec2 = float(tokens[6])
-	        sign2 = geosign(tokens[7],'E','V')
-	        lat = dms2dec(deg1, min1, sec1, sign1)
-	        long = dms2dec(deg2, min2, sec2, sign2)
-	        if sec1 == 0 and sec2 == 0:
-		        log(u"*''W'': [[:%s]] ar putea avea nevoie de actualizarea \
-			        coordonatelor - valoarea secundelor este 0" % title)
+            wikipedia.output(u"*[[%s]] We should ignore parameter 9: %s (%s)" %
+                            (title, tokens[8], link))
+            deg1 = float(tokens[0])
+            min1 = float(tokens[1])
+            sec1 = float(tokens[2])
+            sign1 = geosign(tokens[3],'N','S')
+            deg2 = float(tokens[4])
+            min2 = float(tokens[5])
+            sec2 = float(tokens[6])
+            sign2 = geosign(tokens[7],'E','V')
+            lat = dms2dec(deg1, min1, sec1, sign1)
+            long = dms2dec(deg2, min2, sec2, sign2)
+            if sec1 == 0 and sec2 == 0:
+                log(u"*''W'': [[:%s]] ar putea avea nevoie de actualizarea \
+                    coordonatelor - valoarea secundelor este 0" % title)
         else:
-	        lat1 = float(tokens[0]) * geosign(tokens[1], 'N', 'S')
-	        long1 = float(tokens[2]) * geosign(tokens[3], 'E', 'V')
-	        lat2 = float(tokens[5]) * geosign(tokens[6], 'N', 'S')
-	        long2 = float(tokens[7]) * geosign(tokens[8], 'E', 'V')
-	        if lat1 == 0 or long1 == 0 or lat2 == 0 or long2 == 0:
-		        #TODO: one of them is 0; this is also true for equator and GMT
-		        log(u"*''E'': [[:%s]] Problemă (2) cu legătura Geohack: - \
-			        una dintre coordonatele de bounding box e 0: %s" %
-			        (title, link))
-		        return 0,0
-	        lat = (lat1 + lat2) / 2
-	        long = (long1 + long2) / 2
+            lat1 = float(tokens[0]) * geosign(tokens[1], 'N', 'S')
+            long1 = float(tokens[2]) * geosign(tokens[3], 'E', 'V')
+            lat2 = float(tokens[5]) * geosign(tokens[6], 'N', 'S')
+            long2 = float(tokens[7]) * geosign(tokens[8], 'E', 'V')
+            if lat1 == 0 or long1 == 0 or lat2 == 0 or long2 == 0:
+                #TODO: one of them is 0; this is also true for equator and GMT
+                log(u"*''E'': [[:%s]] Problemă (2) cu legătura Geohack: - \
+                    una dintre coordonatele de bounding box e 0: %s" %
+                    (title, link))
+                return 0,0
+            lat = (lat1 + lat2) / 2
+            long = (long1 + long2) / 2
     elif numElem == 8: # D_M_S_N_D_M_S_E
         deg1 = float(tokens[0])
         min1 = float(tokens[1])
@@ -195,8 +196,8 @@ def parseGeohackLinks(page):
         lat = dms2dec(deg1, min1, sec1, sign1)
         long = dms2dec(deg2, min2, sec2, sign2)
         if sec1 == 0 and sec2 == 0:
-	        log(u"*''W'': [[:%s]] ar putea avea nevoie de actualizarea" \
-		        u" coordonatelor - valoarea secundelor este 0" % title)
+            log(u"*''W'': [[:%s]] ar putea avea nevoie de actualizarea" \
+                u" coordonatelor - valoarea secundelor este 0" % title)
     elif numElem == 6: # D_M_N_D_M_E
         deg1 = float(tokens[0])
         min1 = float(tokens[1])
@@ -209,7 +210,7 @@ def parseGeohackLinks(page):
         lat = dms2dec(deg1, min1, sec1, sign1)
         long = dms2dec(deg2, min2, sec2, sign2)
         log(u"*''E'': [[:%s]] are nevoie de actualizarea coordonatelor" \
-	        u" nu sunt disponibile secundele" % title)
+            u" nu sunt disponibile secundele" % title)
     elif numElem == 4: # D_N_D_E
         deg1 = float(tokens[0])
         sign1 = geosign(tokens[1],'N','S')
@@ -219,39 +220,41 @@ def parseGeohackLinks(page):
         long = sign2 * deg2
     else:
         log(u"*''E'': [[:%s]] Problemă (3) cu legătura Geohack: nu pot" \
-	        u" identifica nicio coordonată: %s" % (title, link))
+            u" identifica nicio coordonată: %s" % (title, link))
         return 0,0
     if lat < 43 or lat > 48.25 or long < 20 or long > 29.67:
         log(u"*''E'': [[:%s]] Coordonate invalide pentru România: %f,%f" \
-	        u" (extrase din %s)" % (title, lat, long, link))
+            u" (extrase din %s)" % (title, lat, long, link))
         return 0,0
     return lat,long
 
 def checkAllCodes(result, title, logMsg = True):
     if len(result) == 0:
-	if logMsg:
-	    log(u"*''E'': [[:%s]] nu conține niciun cod LMI valid" % title)
-        return None
+        if logMsg:
+            log(u"*''E'': [[:%s]] nu conține niciun cod LMI valid" % title)
+            return None
     elif len(result) > 1:
         code = result[0][0]
         if (code.rfind('.') > -1):
-	    c1 = code[-8:code.rfind('.')]
+            c1 = code[-8:code.rfind('.')]
         else:
-	    c1 = code[-5:]
+            c1 = code[-5:]
         for res in result:
-	    if code != res[0]:
-		point = res[0].rfind('.')
-		if (point > -1):
-		    c2 = res[0][-8:point]
-		else:
-		    c2 = res[0][-5:]
-		if c1 != c2: #they're NOT sub-monuments
-		    if logMsg:
-			log(u"*''W'': [[:%s]] conține mai multe coduri LMI" \
-			u" distincte: %s, %s. În mod normal, acest lucru" \
-			u" este o greșeală" % (title, code, res[0]))
-		return ""
-    return result[0][0]#first regular expression
+            if code != res[0]:
+                point = res[0].rfind('.')
+                if (point > -1):
+                    c2 = res[0][-8:point]
+                else:
+                    c2 = res[0][-5:]
+                if c1 != c2: #they're NOT sub-monuments
+                    if logMsg:
+                        log(u"*''W'': [[:%s]] conține mai multe coduri LMI" \
+                        u" distincte: %s, %s. În mod normal, acest lucru" \
+                        u" este o greșeală" % (title, code, res[0]))
+                    return ""
+        return code
+    else:
+        return result[0][0]#first regular expression
 
 def processArticle(text, page, conf):
     title = page.title()
@@ -259,14 +262,16 @@ def processArticle(text, page, conf):
     global codeRegexp
     code = checkAllCodes(re.findall(codeRegexp, text), title)
     if code is None: #no valid code
+        wikipedia.output("No valid code in page " + title)
         return
     elif code == "": #more than one code, juse use the one that is marked as {{codLMI|code}}
         code = checkAllCodes(re.findall(templateRegexp, text), title, False)
         if code is None or code == "": # either no code or more than one code is marked; just ignore
-            return
+               wikipedia.output("Too many codes in page " + title)
+               return
     if re.search(errorRegexp, text) <> None:
         log(u"*''E'': [[:%s]] a fost marcat de un editor ca având o eroare în" \
-	        " codul LMI %s" % (title, code))
+            " codul LMI %s" % (title, code))
         return
 
     if qualityRegexp <> None and re.search(qualityRegexp, text) <> None:
@@ -278,45 +283,54 @@ def processArticle(text, page, conf):
 
     author = None
     image = None
+
     for box in conf['infoboxes']:
         tl = strainu.extractTemplate(text, box['name'])
         if tl == None:
-	        continue
+            continue
         (_dict, _keys) = strainu.tl2Dict(tl)
         if box['author'] in _dict:
-	        author = _dict[box['author']]
-	        #if we're using some strange attribution, mention it explicitly
-	        if box['author'] <> "arhitect" and box['author'] <> "artist":
-	            author += " (" + box['author'] + ")"
-	        #wikipedia.output(author)
+            author = _dict[box['author']]
+            #if we're using some strange attribution, mention it explicitly
+            if box['author'] <> "arhitect" and box['author'] <> "artist":
+                author += " (" + box['author'] + ")"
+            #wikipedia.output(author)
         if box['image'] in _dict:
-	        #TODO:prefix with the namespace
-	        image = _dict[box['image']]
-	        #wikipedia.output(image)
+            #TODO:prefix with the namespace
+            image = _dict[box['image']]
+            #wikipedia.output(image)
         if author <> None and image <> None:
-	        break # stop only if we have all the information we need
+            break # stop only if we have all the information we need
 
     if image == None:
-	# if there are images in the article, use the first image
+    # if there are images in the article, use the first image
     # I'm deliberately skipping images in templates (they have been treated
-	# above) and galleries, which usually contain non-selected images
+    # above) and galleries, which usually contain non-selected images
         for img in page.linkedPages(withImageLinks = True):
-	        if img.isImage() and image == None:
-		        image = img.title()
-		        break
+            if img.isImage() and image == None:
+                image = img.title()
+                break
 
     if code in fullDict:
+        for elem in fullDict[code]:
+            if elem['name'] == title:
+                fullDict[code].remove(elem)
+                break
         fullDict[code].append({'name': title,
-				'project': page.site().language(),
-				'lat': lat, 'long': long,
-				'author': author, 'image': image,
-				'quality': quality})
+                'project': page.site().language(),
+                'lat': lat, 'long': long,
+                'author': author, 'image': image,
+                'quality': quality,
+                'lastedit': page.editTime(),
+                'code': code})
     else:
         fullDict[code] = [{'name': title,
-			    'project': page.site().language(),
-			    'lat': lat, 'long': long,
-			    'author': author, 'image': image,
-			    'quality': quality}]
+                'project': page.site().language(),
+                'lat': lat, 'long': long,
+                'author': author, 'image': image,
+                'quality': quality,
+                'lastedit': page.editTime(),
+                'code': code}]
 
 def processImagePage(text, page, conf):
     pass
@@ -327,10 +341,10 @@ def main():
 
     for arg in wikipedia.handleArgs():
         if arg.startswith('-lang:'):
-	        lang = arg [len('-lang:'):]
-	        user.mylang = lang
+            lang = arg [len('-lang:'):]
+            user.mylang = lang
         if arg.startswith('-family'):
-	        user.family = arg [len('-family:'):]
+            user.family = arg [len('-family:'):]
 
     site = wikipedia.getSite()
     lang = site.language()
@@ -341,7 +355,7 @@ def main():
     langOpt = options.get(lang)
 
     rowTemplate = wikipedia.Page(site, u'%s:%s' % (site.namespace(10), \
-						        langOpt.get('codeTemplate')))
+                                langOpt.get('codeTemplate')))
     global _log
     global fullDict
     global qualityRegexp
@@ -358,24 +372,50 @@ def main():
 
     for namespace in langOpt.get('namespaces'):
         transGen = pagegenerators.ReferringPageGenerator(rowTemplate,
-							        onlyTemplateInclusion=True)
+                                    onlyTemplateInclusion=True)
         filteredGen = pagegenerators.NamespaceFilterPageGenerator(transGen,
-							        [namespace], site)
-        pregenerator = pagegenerators.PreloadingGenerator(filteredGen, 250)
+                                    [namespace], site)
+        pregenerator = pagegenerators.PreloadingGenerator(filteredGen, 125)
 
         #page = wikipedia.Page(site, "File:Biserica_Sf._Maria,_sat_Drumul_Carului,_'La_Cetate'-Gradistea._Moeciu,_jud._BRASOV.jpg")
         count = 0
-        for page in pregenerator:
-	        if page.exists() and not page.isRedirectPage():
-		        # Do some checking
-		        processArticle(page.get(), page, langOpt)
-		        count += 1
-        print count
         if namespace == 0:
-	        namespaceName = ""
+            namespaceName = ""
         else:
-	        namespaceName = "_" + site.namespace(namespace)
-        f = open(lang + namespaceName + "_pages.json", "w+")
+            namespaceName = "_" + site.namespace(namespace)
+        filename = lang + namespaceName + "_pages.json"
+        f = open(filename, "r+")
+        jsonFile = json.load(f)
+        f.close();
+        for page in pregenerator:
+            if page.exists() and not page.isRedirectPage():
+                content = []
+                vallist = jsonFile.values() # extract list of values
+                if len(vallist):
+                    #print vallist
+                    content = [vallist[i][j] for i in range(len(vallist)) for j in range(len(vallist[i])) if vallist[i][j]["name"] == page.title()]
+                #print content
+                if len(content):
+                    content = content[0]
+                    if 'lastedit' in content:
+                        lastedit = content['lastedit']
+                    else:
+                        lastedit = 0
+                    if 'code' in content:
+                        code = content['code']
+                    else:
+                        code = 0
+                    if page.editTime() <= lastedit:
+                        if code in fullDict:
+                            fullDict[code].append(content)
+                        else:
+                            fullDict[code] = [content]
+                        wikipedia.output(u'Skipping "%s"' % page.title())
+                        continue
+                processArticle(page.get(), page, langOpt)
+                count += 1
+        print count
+        f = open(filename, "w+")
         json.dump(fullDict, f, indent = 2)
         f.close();
         fullDict = {}
