c25e9a879c0a63ad890b3e4f7f9162631e8c7dad
strainu10@gmail.com <strainu10@gmail.com@04516ab2-3ca3-11de-be05-5f7a86268029>
2012-10-16 20:56:39 +0000

    Improve template parsing.
    Allow several consecutive internal templates. Add some commented debug
    logs. Corretly handle templates in refs.

--- robots/python/pywikipedia/strainu_functions.py
+++ robots/python/pywikipedia/strainu_functions.py
@@ -32,15 +32,19 @@ def extractTemplate(text, template):
     while open < close and open <> -1:
         open = text.find("{")
         close = text.find("}") + 1
-        if open > close or open == -1:
+        #print "one" + str(open) + " " + str(close)
+        if open >= close or open == -1:
             while innerTlCount > 0:
-                #print str(close) + " "  + str(innerTlCount)
+                #print "two" + str(close) + " "  + str(innerTlCount)
                 close = text.find("}", close) + 1
+                #print "three" + str(close)
                 innerTlCount -= 1
         if open < close:
+            #print text[open + 1:close]
             innerTlCount += text[open + 1:close].count('{')
-        #print str(open) + " " + str(close) + " " + str(innerTlCount)
+        #print "four" + str(open) + " " + str(close) + " " + str(innerTlCount)
         text = text[close:]
+        #print text
     tl = tl.replace(text[1:], "") #the [1:] is because we want to grab the second '}'
     return tl
 
@@ -55,24 +59,27 @@ def tl2Dict(template):
     marker = "@@"
     Rtemplate = re.compile(ur'{{(msg:)?(?P<name>[^{\|]+?)(\|(?P<params>[^{]+?))?}}')
     Rmath = re.compile(ur'<math>[^<]+</math>')
-    Rref = re.compile(ur'<ref>[^<]+</ref>')
+    Rref = re.compile(ur'<ref.*?>[^<]+</ref>')
     Rmarker = re.compile(ur'%s(\d+)%s' % (marker, marker))
     count = 0
     intern = {}
     for m in Rref.finditer(template):
         count += 1
         text = m.group()
+        #print "ref " + text
         template = template.replace(text, '%s%d%s' % (marker, count, marker))
         intern[count] = text
     for m in Rmath.finditer(template):
         count += 1
         text = m.group()
+        #print "math " + text
         template = template.replace(text, '%s%d%s' % (marker, count, marker))
         intern[count] = text
     while Rtemplate.search(template[2:]) is not None:
         for m in Rtemplate.finditer(template):
             count += 1
             text = m.group()
+            #print "template " + text
             text2 = text
             for m2 in Rmarker.finditer(text):
                 text2 = text2.replace(m2.group(), intern[int(m2.group(1))])
@@ -80,34 +87,35 @@ def tl2Dict(template):
             intern[count] = text2
     _dict = {}
     _keyList = []
-    template = re.sub(r'(\r|\n)', '', template)
+    #print intern
+    template = re.sub(ur'(\r|\n)', '', template)
     template = template[0:len(template)-2]#get rid of '}}'
     params = template.split('|')
     key = u""
     value = unicode("",'utf8')
     for line in params:
-        line = line.split('=')
+        line = line.split(u'=')
         #pywikibot.output(str(line))
         if (len(line) > 1):
-            key = line[0].encode("utf8")
-            key = re.sub(r'\s', '', key)
+            key = line[0]#.encode("utf8")
+            key = re.sub(ur'\s', '', key)
             value = u"=".join(line[1:])
             _dict[key] = value
             if not key in _keyList:
                 _keyList.append(key)
-        elif line[0].startswith('{{') and not "_name" in _dict: #name of the template
+        elif line[0].startswith(u'{{') and not u"_name" in _dict: #name of the template
             #pywikibot.output("Name: " + line[0][2:])
-            _dict["_name"] = line[0][2:]
+            _dict[u"_name"] = line[0][2:]
             if not key in _keyList:
-                _keyList.append("_name")
-        elif line[0] != "" and key != "":#the first line might not begin with {{
-            _dict[key] = _dict[key] + "|" + line[0]
+                _keyList.append(u"_name")
+        elif line[0] != u"" and key != u"":#the first line might not begin with {{
+            _dict[key] = _dict[key] + u"|" + line[0]
             if not key in _keyList:
                 _keyList.append(key)
     for key, value in _dict.items():
-        match = Rmarker.search(value)
-        if match:
-            count = int(match.group(1))
+        matches = Rmarker.findall(value)
+        for match in matches:
+            count = int(match)
             value = value.replace(u'%s%d%s' % (marker, count, marker), intern[count])
             _dict[key] = value
     #print _dict
