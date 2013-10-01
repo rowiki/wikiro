d3ea058d20afe1e69c6c000d2087a9a0f5be4bca
andrei.stroe@gmail.com <andrei.stroe@gmail.com@04516ab2-3ca3-11de-be05-5f7a86268029>
2013-10-01 10:57:51 +0000

    Beginning parser for Hungary

---
+++ trunk/robots/java/PopulationDB/src/info/astroe/populationdb/hu/model/Nationality.java
@@ -0,0 +1,37 @@
+package info.astroe.populationdb.hu.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "nationalitate")
+public class Nationality {
+    private int id;
+    private String nume;
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    @Column(name = "nume")
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(final String nume) {
+        this.nume = nume;
+    }
+}
