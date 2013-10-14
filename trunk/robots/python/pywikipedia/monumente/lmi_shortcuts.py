800989f1bdd7d163196decb85cb14af3a876cca9
strainu10@gmail.com <strainu10@gmail.com@04516ab2-3ca3-11de-be05-5f7a86268029>
2013-10-14 21:06:38 +0000

    Put the shortcut link to the list, add a special page for article

--- trunk/robots/python/pywikipedia/monumente/lmi_shortcuts.py
+++ trunk/robots/python/pywikipedia/monumente/lmi_shortcuts.py
@@ -3,12 +3,14 @@
 
 import sys, json
 sys.path.append("..")
-import wikipedia
+import pywikibot
+from pywikibot import pagegenerators
+from pywikibot import config as user
 '''
 This script creates LMI links in the Cod namespace on ro.wp basted on 
 the monument database.
 
-See [[Wikipedia:Coduri]] (ro) for details on the Cod namespace.
+See [[ro:Wikipedia:Coduri]] (ro) for details on the Cod namespace.
 '''
 
 def split_code(code):
@@ -20,43 +22,44 @@ def split_code(code):
 
 def main():
 	f = open("lmi_db.json", "r+")
-	wikipedia.output("Reading database file...")
+	pywikibot.output("Reading database file...")
 	db = json.load(f)
-	wikipedia.output("...done")
+	pywikibot.output("...done")
 	f.close();
 	
 	f = open("ro_pages.json", "r+")
-	wikipedia.output("Reading articles file...")
+	pywikibot.output("Reading articles file...")
 	pages_ro = json.load(f)
-	wikipedia.output("...done")
+	pywikibot.output("...done")
 	f.close();
 	
-	site = wikipedia.getSite()
+	site = pywikibot.getSite()
 	
-	for code in pages_ro:
-		page = wikipedia.Page(site, u"Cod:LMI:" + code)
-		wikipedia.output(page.title())
-		#if page.exists() and not page.isRedirect():
-		#		wikipedia.output(u"Page %s is not a redirect" % page.title())
-		#else:
-		page.put(u"#redirecteaza[[%s]]" % pages_ro[code][0]["name"], "Redirecting code to the Wikipedia article")
+	#for code in pages_ro:
+	#	page = pywikibot.Page(site, u"Cod:LMI:" + code + "/articol")
+	#	pywikibot.output(page.title())
+	#	#if page.exists() and not page.isRedirect():
+	#	#		pywikibot.output(u"Page %s is not a redirect" % page.title())
+	#	#else:
+	#	if not page.exists():
+	#		page.put(u"#redirecteaza[[%s]]" % pages_ro[code][0]["name"], "Redirecting code to the Wikipedia article")
 			
 	for monument in db:
 		if monument["Cod"] in pages_ro:
 			continue
-		page = wikipedia.Page(site, u"Cod:LMI:" + monument["Cod"])
-		wikipedia.output(page.title())
+		page = pywikibot.Page(site, u"Cod:LMI:" + monument["Cod"])
+		pywikibot.output(page.title())
 
-		source_page = wikipedia.url2link(monument["source"][monument["source"].find(u'=')+1:monument["source"].find(u'&')], site, site)
-		#wikipedia.output(source_page)
-		source_page = wikipedia.Page(site, source_page)
+		source_page = monument["source"][monument["source"].find(u'=')+1:monument["source"].find(u'&')]
+		#pywikibot.output(source_page)
+		source_page = pywikibot.Page(site, source_page)
 		page_text = u"#redirect [[{0}#{1}]]".format(source_page.title(), monument["Cod"])
 		if not page.exists() or page.get(False, True) <> page_text:
-			page.put(page_text, "Redirecting code to the Wikipedia article")
+			page.put(page_text, "Redirecting code to the Wikipedia list")
 
 if __name__ == "__main__":
 	try:
 		main()
 	finally:
-		wikipedia.stopme()
+		pywikibot.stopme()
 
