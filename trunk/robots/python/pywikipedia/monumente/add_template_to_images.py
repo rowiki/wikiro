4fb5a9fd6c2f53a8c3c5d3590e48b3f38f3f38ab
strainu10@gmail.com <strainu10@gmail.com@04516ab2-3ca3-11de-be05-5f7a86268029>
2013-10-14 21:08:14 +0000

    Add a function which adds the code from cat to the images

--- trunk/robots/python/pywikipedia/monumente/add_template_to_images.py
+++ trunk/robots/python/pywikipedia/monumente/add_template_to_images.py
@@ -10,6 +10,7 @@ sys.path.append("..")
 import pywikibot
 from pywikibot import pagegenerators
 from pywikibot import config as user
+from pywikibot import catlib as catlib
 
 
 _log = "link2.err.log"
@@ -26,19 +27,12 @@ def closeLog():
 def log(string):
 	pywikibot.output(string.encode("utf8") + "\n")
 	_flog.write(string.encode("utf8") + "\n")
-
-def main():
-	initLog()
-	f = open("lmi_db.json", "r+")
-	pywikibot.output("Reading database file...")
-	db = json.load(f)
-	pywikibot.output("...done")
-	f.close();
 	
+def list2commons(db):
 	#this is the big loop that should only happen once
 	for monument in db:
 		rawCode = monument["Cod"].strip()
-		regexp = re.compile("(([a-z]{1,2})-(i|ii|iii|iv)-([a-z])-([a-z])-([0-9]{5}(\.[0-9]{2})?))", re.I)
+		regexp = re.compile("(([a-z]{1,2})-(i|ii|iii|iv)-([a-z])-([a-z])-([0-9]{5}(\.[0-9]{2,3})?))", re.I)
 		result = re.findall(regexp, rawCode)
 		if len(result) > 0:
 			code = result[0][0]
@@ -71,7 +65,7 @@ def main():
 				found = 1
 				if code <> str(params[0]):
 					log(u"W: Nepotrivire între codul din listă (%s) și unul din codurile din imagine (%s)" % (code, params[0]))
-			elif unicode(template) == u"Information":
+			elif unicode(template.title(withNamespace = False)) == u"Information":
 				info_params = params
 				#print params
 				
@@ -94,8 +88,79 @@ def main():
 					page.put(text, comment="Adding {{tl|Monument istoric}}")
 				else:
 					log(u"E: Imaginea pentru %s are formatul {{f|information}} dar nu și o descriere" % code)
+
+def cat2commons(cat):
+	start = False
+	for code in cat:
+		if len(cat[code]) > 1:
+			log(u"E: Există mai multe categorii pentru %s" % code)
+			continue
+		name = cat[code][0]["name"]
+		if name == u"Category:Cetatea Oradea":
+			start = True
+			continue
+		if not start:
+			continue
+		pywikibot.output(u"Working on cat %s" % name)
+		site = pywikibot.getSite('commons', 'commons')
+		transGen = pagegenerators.CategorizedPageGenerator(catlib.Category(site, name))
+		filteredGen = pagegenerators.NamespaceFilterPageGenerator(transGen, [6], site)
+		pregenerator = pagegenerators.PreloadingGenerator(filteredGen, 125)
+		for page in pregenerator:
+			if page.isRedirectPage():
+				page = page.getRedirectTarget()
+			pywikibot.output(u"Working on file %s" % page.title())
+			text = page.get()
+			templates = page.templatesWithParams()
+			found = False
+			info_params = None
+			for (template, params) in templates:
+				if unicode(template.title(withNamespace = False)) == u"Monument istoric":
+					found = True
+					print params
+					if code <> str(params[0]):
+						log(u"W: Nepotrivire între codul din categorie (%s) și unul din codurile din imagine (%s)" % (code, params[0]))
+				elif unicode(template.title(withNamespace = False)) == u"Information" or unicode(template.title(withNamespace = False)) == u"Artwork":
+					info_params = params
+					#print params
+			
+			if found:
+				continue   
+			addedText = "{{Monument istoric|%s}}" % code
+			if info_params == None:
+				text = addedText + "\n" + text
+				#print text
+				page.put(text, comment="Adding {{tl|Monument istoric}}")
+				continue
+			else:
+				description = None
+			for param in info_params:
+				pos = unicode(param.lower()).find(u"description")
+				if pos == 0 or (pos > 0 and param[0:pos].strip() == ""):
+					description = unicode(param)
+					break
+			if description:
+				text = text.replace(description, description + addedText + "\n")
+				#print text
+				page.put(text, comment="Adding {{tl|Monument istoric}}")
+			else:
+				log(u"E: Imaginea pentru %s are formatul {{f|information}} dar nu și o descriere" % code)
+
+def main():
+	initLog()
+	f = open("lmi_db.json", "r+")
+	pywikibot.output("Reading database file...")
+	db = json.load(f)
+	pywikibot.output("...done")
+	f.close();
+	f = open("commons_Category_pages.json", "r+")
+	pywikibot.output("Reading commons categories file...")
+	cat = json.load(f)
+	pywikibot.output("...done")
+	f.close();
+	#list2commons(db)
+	cat2commons(cat)
 	closeLog()
-	
 
 if __name__ == "__main__":
 	try:
