b70ca649a107c30429b546ede5b9505cde0e5de9
andrei.stroe <andrei.stroe@04516ab2-3ca3-11de-be05-5f7a86268029>
2014-06-14 20:10:03 +0000

    retriable executor

--- trunk/robots/java/PopulationDB/src/org/wikipedia/ro/populationdb/util/WikiEditExecutor.java
+++ trunk/robots/java/PopulationDB/src/org/wikipedia/ro/populationdb/util/WikiEditExecutor.java
@@ -9,21 +9,37 @@ import org.wikipedia.Wiki;
 
 public class WikiEditExecutor implements Executor {
 
-    private Wiki wiki;
-    private Wikibase dwiki;
+	private Wiki wiki;
+	private Wikibase dwiki;
 
-    public WikiEditExecutor(Wiki wiki, Wikibase dwiki) {
-        super();
-        this.wiki = wiki;
-        this.dwiki = dwiki;
-    }
+	public WikiEditExecutor(Wiki wiki, Wikibase dwiki) {
+		super();
+		this.wiki = wiki;
+		this.dwiki = dwiki;
+	}
 
-    public void save(String title, String text, String comment) throws LoginException, IOException {
-        wiki.edit(title, text, comment);
-    }
+	public void save(String title, String text, String comment)
+			throws LoginException, IOException {
+		boolean success = true;
+		do {
+			try {
+				wiki.edit(title, text, comment);
+			} catch (Throwable t) {
+				success = false;
+			}
+		} while (!success);
+	}
 
-    public void link(String fromwiki, String fromtitle, String towiki, String totitle) throws Exception {
-        dwiki.linkPages(fromwiki, fromtitle, towiki, totitle);
-    }
+	public void link(String fromwiki, String fromtitle, String towiki,
+			String totitle) throws Exception {
+		boolean success = true;
+		do {
+			try {
+				dwiki.linkPages(fromwiki, fromtitle, towiki, totitle);
+			} catch (Throwable t) {
+				success = false;
+			}
+		} while (!success);
+	}
 
 }
