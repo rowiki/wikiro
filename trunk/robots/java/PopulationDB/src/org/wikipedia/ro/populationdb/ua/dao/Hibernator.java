e5e884287b4af8650a5ce6a20ac10fd03e06b00b
andrei.stroe@gmail.com <andrei.stroe@gmail.com@04516ab2-3ca3-11de-be05-5f7a86268029>
2014-07-28 11:55:57 +0000

    Some cleanup and Miskrada functionality

--- trunk/robots/java/PopulationDB/src/org/wikipedia/ro/populationdb/ua/dao/Hibernator.java
+++ trunk/robots/java/PopulationDB/src/org/wikipedia/ro/populationdb/ua/dao/Hibernator.java
@@ -4,9 +4,15 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.wikipedia.ro.populationdb.ua.model.Commune;
+import org.wikipedia.ro.populationdb.ua.model.Raion;
+import org.wikipedia.ro.populationdb.ua.model.Region;
 import org.wikipedia.ro.populationdb.util.HibernateUtil;
 
 public class Hibernator {
@@ -33,4 +39,68 @@ public class Hibernator {
             sessionFactory.close();
         }
     }
+
+    public Region getRegionByTransliteratedName(final String needle) {
+        final Session ses = sessionFactory.getCurrentSession();
+        final Criteria crit = ses.createCriteria(Region.class).add(Restrictions.eq("transliteratedName", needle));
+        final List rez = crit.list();
+        if (0 < rez.size()) {
+            return (Region) rez.get(0);
+        }
+        return null;
+    }
+
+    public Commune getCommuneByRomanianName(final String needle) {
+        final Session ses = sessionFactory.getCurrentSession();
+        final Criteria crit = ses.createCriteria(Commune.class).add(Restrictions.eq("romanianName", needle));
+        final List rez = crit.list();
+        if (0 < rez.size()) {
+            return (Commune) rez.get(0);
+        }
+        return null;
+    }
+
+    public Commune getCommuneByTransliteratedName(final String needle) {
+        final Session ses = sessionFactory.getCurrentSession();
+        final Criteria crit = ses.createCriteria(Commune.class).add(Restrictions.eq("transliteratedName", needle));
+        final List rez = crit.list();
+        if (0 < rez.size()) {
+            return (Commune) rez.get(0);
+        }
+        return null;
+    }
+
+    public Commune getCommuneByTransliteratedNameAndRaion(final String needle, final Raion raion) {
+        final Session ses = sessionFactory.getCurrentSession();
+        final Criteria crit = ses.createCriteria(Commune.class).add(Restrictions.eq("transliteratedName", needle))
+            .add(Restrictions.eq("raion", raion));
+        final List rez = crit.list();
+        if (0 < rez.size()) {
+            return (Commune) rez.get(0);
+        }
+        return null;
+    }
+
+    public Commune getCommuneByTransliteratedNameAndRaion(final String needle, final Region region) {
+        final Session ses = sessionFactory.getCurrentSession();
+        final Criteria crit = ses.createCriteria(Commune.class).add(Restrictions.eq("transliteratedName", needle))
+            .add(Restrictions.eq("region", region));
+        final List rez = crit.list();
+        if (0 < rez.size()) {
+            return (Commune) rez.get(0);
+        }
+        return null;
+    }
+
+    public Raion getRaionByTransliteratedNameAndRegion(final String needle, final Region region) {
+        final Session ses = sessionFactory.getCurrentSession();
+        final Criteria crit = ses.createCriteria(Raion.class).add(Restrictions.eq("transliteratedName", needle))
+            .add(Restrictions.eqOrIsNull("region", region));
+        final List rez = crit.list();
+        if (0 < rez.size()) {
+            return (Raion) rez.get(0);
+        }
+        return null;
+    }
+
 }
