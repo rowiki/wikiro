b2487273bc94defb0e4b27b8182c5d511d8a1c91
strainu10@gmail.com <strainu10@gmail.com@04516ab2-3ca3-11de-be05-5f7a86268029>
2014-07-28 21:06:13 +0000

    Add capability to add template for a single category

--- robots/python/pywikipedia/monumente/add_template_to_images.py
+++ robots/python/pywikipedia/monumente/add_template_to_images.py
@@ -27,6 +27,31 @@ def closeLog():
 def log(string):
 	pywikibot.output(string.encode("utf8") + "\n")
 	_flog.write(string.encode("utf8") + "\n")
+
+def commonsImage(image):
+	site = pywikibot.getSite('commons', 'commons')
+        try:
+                page = pywikibot.Page(site, image)
+		if not page.exists():
+			return None
+                if page.isRedirectPage():
+                        page = page.getRedirectTarget()
+
+        except Exception:
+                return None
+	return page
+
+def localImage(image):
+	site = pywikibot.getSite('ro', 'wikipedia')
+	try:
+		page = pywikibot.Page(site, image)
+		if not page.exists():
+			return None
+		if page.isRedirectPage():
+			page = page.getRedirectTarget()
+	except Exception as e:
+		return None
+	return page
 	
 def list2commons(db):
 	#this is the big loop that should only happen once
@@ -46,30 +71,36 @@ def list2commons(db):
 			
 		image = image.replace(u'Fișier', u'File')
 		image = image.replace(u'Imagine', u'File')
-		site = pywikibot.getSite('commons', 'commons')
+		#print image
+		page = commonsImage(image)
+		#print page
+		if page == None:
+                	log("I: Local image [[:%s]] for code %s" % (image, code))
+			page = localImage(image)
+		if page == None:
+			log("E: Local image [[:%s]] for code %s does not exist" % (image, code))
+			continue	
 		try:
-			page = pywikibot.Page(site, image)
-			if page.isRedirectPage():
-				page = page.getRedirectTarget()
 			text = page.get()
 		except Exception:
-			log("I: Local image [[:%s]] for code %s" % (image, code))
 			continue
-			
+		#print page.site
 		templates = page.templatesWithParams()
 		info_params = None
 		found = 0
 		for (template, params) in templates:
 			#print template
-			if unicode(template.title(withNamespace = False)) == u"Monument istoric":
+			if unicode(template.title(withNamespace = False)) == u"Monument istoric" or unicode(template.title(withNamespace = False)) == u"CodLMI":
 				found = 1
 				if code <> str(params[0]):
 					log(u"W: Nepotrivire între codul din listă (%s) și unul din codurile din imagine (%s)" % (code, params[0]))
-			elif unicode(template.title(withNamespace = False)) == u"Information":
-				info_params = params
+			elif unicode(template.title(withNamespace = False)) == u"Information" or unicode(template.title(withNamespace = False)) == u"Informații":
+				pass
+				#info_params = params
 				#print params
 				
 		if not found: # we need to add the template
+			#print text
 			addedText = "{{Monument istoric|%s}}" % code
 			if info_params == None:
 				text = addedText + "\n" + text
@@ -78,19 +109,22 @@ def list2commons(db):
 			else:
 				description = None
 				for param in info_params:
-					pos = unicode(param.lower()).find(u"description")
+					pos = unicode(param.lower()).find(u"descri")
 					if  pos == 0 or (pos > 0 and param[0:pos].strip() == ""):
 						description = unicode(param)
 						break
 				if description:
-					text = text.replace(description, description + addedText + "\n")
+					#print addedText
+					new = description + addedText + "\n"
+					#print new
+					text = text.replace(description, new)
 					#print text
 					page.put(text, comment="Adding {{tl|Monument istoric}}")
 				else:
 					log(u"E: Imaginea pentru %s are formatul {{f|information}} dar nu și o descriere" % code)
 
 def cat2commons(cat):
-	start = False
+	start = True
 	for code in cat:
 		if len(cat[code]) > 1:
 			log(u"E: Există mai multe categorii pentru %s" % code)
@@ -105,7 +139,7 @@ def cat2commons(cat):
 		site = pywikibot.getSite('commons', 'commons')
 		transGen = pagegenerators.CategorizedPageGenerator(catlib.Category(site, name))
 		filteredGen = pagegenerators.NamespaceFilterPageGenerator(transGen, [6], site)
-		pregenerator = pagegenerators.PreloadingGenerator(filteredGen, 125)
+		pregenerator = pagegenerators.PreloadingGenerator(filteredGen, 50)
 		for page in pregenerator:
 			if page.isRedirectPage():
 				page = page.getRedirectTarget()
@@ -121,7 +155,8 @@ def cat2commons(cat):
 					if code <> str(params[0]):
 						log(u"W: Nepotrivire între codul din categorie (%s) și unul din codurile din imagine (%s)" % (code, params[0]))
 				elif unicode(template.title(withNamespace = False)) == u"Information" or unicode(template.title(withNamespace = False)) == u"Artwork":
-					info_params = params
+					pass
+					#info_params = params
 					#print params
 			
 			if found:
@@ -147,6 +182,11 @@ def cat2commons(cat):
 				log(u"E: Imaginea pentru %s are formatul {{f|information}} dar nu și o descriere" % code)
 
 def main():
+	cat = None
+	
+	for arg in pywikibot.handleArgs():
+		if arg.startswith('-cat'):
+			cat = arg [len('-cat:'):]
 	initLog()
 	f = open("lmi_db.json", "r+")
 	pywikibot.output("Reading database file...")
@@ -155,11 +195,16 @@ def main():
 	f.close();
 	f = open("commons_Category_pages.json", "r+")
 	pywikibot.output("Reading commons categories file...")
-	cat = json.load(f)
+	categs = json.load(f)
 	pywikibot.output("...done")
 	f.close();
+	if cat:
+		for code in categs.keys():
+			if categs[code][0][u"name"] != cat:
+				del categs[code]
+	print categs
 	#list2commons(db)
-	cat2commons(cat)
+	cat2commons(categs)
 	closeLog()
 
 if __name__ == "__main__":
