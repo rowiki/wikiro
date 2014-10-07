10347bcd692247dbdb1848462f214bde4138ffd9
nicubunu@gmail.com <nicubunu@gmail.com@04516ab2-3ca3-11de-be05-5f7a86268029>
2014-10-07 11:24:27 +0000

    concurs juniori 2014

--- trunk/web/redirector-qr/index.php
+++ trunk/web/redirector-qr/index.php
@@ -7,9 +7,11 @@ $ua = $_SERVER['HTTP_USER_AGENT'];
 $date= date("Y/m/d:h:i:sa");
 
  switch ($lmi) {
-	 case "BV-II-a-A-11769": $redirect="https://commons.wikimedia.org/wiki/File:Cetatea_Rupea,_judetul_Brasov.jpg";break;
- 	 case "HD-II-m-A-03344": $redirect="https://commons.wikimedia.org/wiki/File:Castelul_Corvinestilor_-_vedere_median-frontala_de_pe_podul_de_acces.jpg";break;
-	 case "AB-II-m-A-00129": $redirect="https://commons.wikimedia.org/wiki/File%3AAlba_Iulia_-_Catedrala_Incoronarii_si_Catedrala_Sfantul_Mihail.jpg";break; 	 
+	 case "SV-II-a-A-05595": $redirect="https://ro.wikipedia.org/wiki/M%C4%83n%C4%83stirea_Sucevi%C8%9Ba";break;
+	 case "SV-II-a-A-05595-1": $redirect="https://ro.wikipedia.org/wiki/M%C4%83n%C4%83stirea_Sucevi%C8%9Ba";break;  
+   case "VN-IV-m-A-06632":   $redirect="https://ro.wikipedia.org/wiki/Mausoleul_de_la_M%C4%83r%C4%83%C8%99e%C8%99ti";break;
+   case "SV-II-m-B-05545":   $redirect="https://commons.wikimedia.org/wiki/File:Galeria_Oamenilor_de_Seama_Falticeni.JPG";break;
+   case "VN-II-m-A-06417":   $redirect="https://commons.wikimedia.org/wiki/File:Tribunalul_Judetean_Vrancea,_Focsani.JPG";break;
          default: $redirect="https://commons.wikimedia.org/wiki/Category:Images_from_Wiki_Loves_Monuments_2014_in_Romania";
 	 } 
 
