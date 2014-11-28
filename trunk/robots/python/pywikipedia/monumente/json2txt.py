1a8ff4e0764e1ba374ceb66dda0ce7ad65d4c5a6
strainu10@gmail.com <strainu10@gmail.com@04516ab2-3ca3-11de-be05-5f7a86268029>
2014-11-28 17:50:28 +0000

    Add configs for all wikis

--- trunk/robots/python/pywikipedia/monumente/json2txt.py
+++ trunk/robots/python/pywikipedia/monumente/json2txt.py
@@ -3,6 +3,9 @@
 
 import json, codecs
 
+import pywikibot
+from pywikibot import config as user
+
 def split_code(code):
         parts = code.split('-')
         if len(parts) < 3:
@@ -10,15 +13,45 @@ def split_code(code):
                 return (None, None, None, None)
         return (parts[0], parts[1], parts[2], parts[3])
 
+config = {
+	'lmiro': {
+		'output': u"rofiles.txt",
+		'input': u"ro_Fișier_pages.json",
+		'lang': u"ro",
+		'family': u"wikipedia",
+	},
+	'lmicommons': {
+		'output': u"monumentfiles.txt",
+		'input': u"commons_File_pages.json",
+		'lang': u"commons",
+		'family': u"commons",
+	},
+	'default': {
+		'output': u"downloadlist.txt",
+		'input': u"img_download",
+		'lang': u"commons",
+		'family': u"commons",
+	},
+}
+
 if __name__ == "__main__":
-	f = codecs.open("commons_File_pages.json", "r+", "utf8")
+	for arg in pywikibot.handleArgs():
+		if arg.startswith('-cfg'):
+			cfg = arg [len('-cfg:'):]
+
+	output      = config[cfg]['output']
+	input    = config[cfg]['input']
+	user.mylang = config[cfg]['lang']
+	user.family = config[cfg]['family']
+
+	f = codecs.open(input, "r+", "utf8")
 	#f = codecs.open("ro_Fișier_pages.json", "r+", "utf8")
-	print ("Reading commons images file...")
+	print ("Reading %s images file..." % cfg)
 	pages_commons = json.load(f)
 	print ("...done")
 	f.close();
 
-	f = codecs.open("monumentfiles.txt", "w+", "utf8")
+	f = codecs.open(output, "w+", "utf8")
 	#f = codecs.open("export_monumenteuitate_20140207.txt", "w+", "utf8")
 	#f = codecs.open("rofiles.txt", "w+", "utf8")
 	for code in pages_commons:
