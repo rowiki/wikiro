35174790eeb9f181ffa505a4951a3be3ba3e3bd8
Andrei Cipu <github@strainu.ro>
2015-09-06 23:56:22 +0300

    Further implement the page generation

--- robots/python/pywikipedia/parliament/person.py
+++ robots/python/pywikipedia/parliament/person.py
@@ -46,11 +46,35 @@ class ElectedPerson(object):
 		function = u""
 		if self.chamber in chambers:
 			function = chambers[self.chamber]
-		begin = u""
+		begin = parliament.niceDate("2012-12-19")
+		if len(self.groups) > 0:
+			begin = parliament.niceDate(self.groups[0].start)
 		end = u""
+		if len(self.groups) > 0 and self.groups[-1].end != None:
+			end = parliament.niceDate(self.groups[-1].end)
 		district = self.district
 		party = u""
 		if len(self.groups):
 			party = self.groups[-1].party
 		
 		return text % (self.name, function, begin, end, district, party, parliament.niceDate(self.birthdate))
+
+	def generateArticle(self):
+		firstParagraph = """'''{{subst:PAGENAME}}''' este un %s român, ales în %d din partea %s. %s
+
+"""
+		mandateEnd = """Mandatul său a încetat pe %s.
+
+"""
+		if len(self.groups) > 3:
+			groups = """În timpului mandatului, a făcut parte din următoarele grupuri parlamentare: %s, %s, %s și %s
+
+"""
+		elif len(self.groups) > 2:
+			groups = """Pe %s a trecut în %s, apoi s-a mutat în %s (pe data de %s).
+
+"""
+		elif len(self.groups) > 1:
+			groups = """În timpului mandatului, a făcut parte din următoarele grupuri parlamentare: %s, %s, %s, %s
+
+"""
