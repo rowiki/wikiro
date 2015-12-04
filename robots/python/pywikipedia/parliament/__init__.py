e9bb7134a402c5d0bae1b9877083d93ee0ced9a0
Andrei Cipu <github@strainu.ro>
2015-12-05 00:45:45 +0200

    [Parliament]Incomplete dates should be left as such by niceDate

--- robots/python/pywikipedia/parliament/__init__.py
+++ robots/python/pywikipedia/parliament/__init__.py
@@ -33,7 +33,7 @@ months_names = {
 def niceDate(date):
 	_list = date.split("-")
 	if len(_list) < 3:
-		return u""
+		return date
 	return u" ".join([ "%d" % int(_list[2]), months_names[int(_list[1])], _list[0]]) 
 
 def allCommas(s):
