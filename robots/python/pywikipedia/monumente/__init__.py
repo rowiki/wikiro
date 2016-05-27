251f0a44dc3bc0b96ca9695b8d81ec139ecc6b77
Andrei Cipu <github@strainu.ro>
2016-05-27 20:36:33 +0300

    Transform "monumente" folder in python module

---
+++ robots/python/pywikipedia/monumente/__init__.py
@@ -0,0 +1,18 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8  -*-
+
+#
+# (C) Strainu, 2012-2015
+#
+# Distributed under the terms of the GPLv2 license.
+#
+#
+
+
+def filterOne(contents, countryconfig):
+	'''
+	Any kind of filtering of the data of a monument should happen in this functions
+	'''
+	#if countryconfig.get('idField') == 'ID': #WLEMD
+	#	contents[countryconfig.get('idField')] = "-".join([u"MD", contents['raion'], contents['tip'], "%03d" % int(contents['ID'])])
+	return contents
