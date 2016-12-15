ef1580cc198e8e51b878f3d13916c3c33fcd1029
Andrei Stroe <andreistroe@users.noreply.github.com>
2016-12-15 12:48:36 +0200

    add Gutenberg to ignored prefixes

--- robots/java/PopulationDB/src/org/wikipedia/ro/astroe/operations/ReplaceCrossLinkWithIll.java
+++ robots/java/PopulationDB/src/org/wikipedia/ro/astroe/operations/ReplaceCrossLinkWithIll.java
@@ -194,7 +194,7 @@ public class ReplaceCrossLinkWithIll implements WikiOperation {
             }
             if (startsWithAny(lowerCase(articleLink), "google:", "wiktionary:", "iarchive:", "file:", "fișier:", "image:",
                 "imagine:", "categorie:", "category:", "arxiv:", "openlibrary:", "s:", "imdbname:", "c:file:", "doi:",
-                "bibcode:", "imdbtitle:", "foldoc:")) {
+                "bibcode:", "imdbtitle:", "foldoc:", "gutenberg:")) {
                 continue;
             }
             articleTitle = defaultString(targetWiki.resolveRedirect(articleTitle), articleTitle);
