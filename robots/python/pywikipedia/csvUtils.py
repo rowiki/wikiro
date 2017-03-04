fe085e8877c06ba7d19c71469acc2206514cc90d
Andrei Cipu <github@strainu.ro>
2017-03-04 13:18:15 +0200

    csvUtils: Add configurable delimiter

--- robots/python/pywikipedia/csvUtils.py
+++ robots/python/pywikipedia/csvUtils.py
@@ -3,13 +3,13 @@
 
 import json, csv, codecs, collections
 
-def csvToJson( inFile, outFile=None, field=u"Cod"):
+def csvToJson( inFile, outFile=None, field=u"Cod", delimiter=","):
     out = None;
 
     with open( inFile, 'r') as csvFile:
         #Note this reads the first line as the keys we can add specific keys with:
         #csv.DictReader( csvFile, fieldnames=<LIST HERE>, restkey=None, restval=None, )
-        csvDict = csv.DictReader( csvFile, restkey=None, restval=None, )
+        csvDict = csv.DictReader( csvFile, restkey=None, restval=None, delimiter=delimiter )
         out = collections.OrderedDict({})
         for row in csvDict:
             if field not in row:
