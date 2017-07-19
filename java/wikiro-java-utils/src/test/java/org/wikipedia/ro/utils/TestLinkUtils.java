b7baf0d23087d9a9dd1531a1c7feca384cc4dfbb
Andrei Stroe <andreistroe@users.noreply.github.com>
2017-07-19 11:02:35 +0300

    utility to build links

---
+++ java/wikiro-java-utils/src/test/java/org/wikipedia/ro/utils/TestLinkUtils.java
@@ -0,0 +1,17 @@
+package org.wikipedia.ro.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestLinkUtils {
+    @Test
+    public void testCreateLink() {
+        
+        Assert.assertEquals("[[gigibobo]]", LinkUtils.createLink("gigibobo", "gigibobo"));
+        Assert.assertEquals("[[gigibobo]]", LinkUtils.createLink("gigibobo", null));
+        Assert.assertEquals("[[gigibobo|bobogigi]]", LinkUtils.createLink("gigibobo", "bobogigi"));
+        Assert.assertEquals("bobogigi", LinkUtils.createLink(null, "bobogigi"));
+        Assert.assertNull(LinkUtils.createLink(null, null));
+        
+    }
+}
