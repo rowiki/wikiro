8a3606de79ab68f0e7d10fd61194db3b8d30eeaf
Strainu <github@strainu.ro>
2017-11-17 00:14:16 +0200

    Redirect1968: handle double redirects

--- robots/python/pywikipedia/localitati/redirect_1968.py
+++ robots/python/pywikipedia/localitati/redirect_1968.py
@@ -9,9 +9,16 @@ def checkPage(title):
         return page
     return None
 
+def forwardPage(page):
+    if page.isRedirectPage():
+        return page.getRedirectTarget()
+    else:
+        return page
+
 def processLine(county, oldName, oldCommune, newName, newCommune):
     redirect = checkPage(", ".join([oldName, county]))
-    if redirect:
+    if redirect and (not redirect.isRedirectPage() or redirect.userName() != "Strainubot"):
+        pywikibot.output(redirect.userName())
         pywikibot.output("%s already exists" % redirect.title())
         return
     title = checkPage("%s (%s), %s" % (newName, newCommune, county))
@@ -22,6 +29,7 @@ def processLine(county, oldName, oldCommune, newName, newCommune):
     if not title:
         pywikibot.output("%s does not exist" % newName)
         return
+    title = forwardPage(title)
     redirect = pywikibot.Page(pywikibot.Site(), ", ".join([oldName, county]))
     text = u"#redirect [[%s]]" % title.title()
     desc = u"Redirectez satul %s spre satul cu care a fost unit prin legea 2/1968 ([[%s]]) #satedisparute1968" % (redirect.title(), title.title())
@@ -31,21 +39,30 @@ def processLine(county, oldName, oldCommune, newName, newCommune):
     if oldCommune != newCommune:
         redirect = checkPage("Comuna %s, %s" % (oldCommune, county))
         title = checkPage("Comuna %s, %s" % (newCommune, county))
-        if not title or redirect:
-            pywikibot.output("Cannot redirect coomune")
+        if not title:
+            pywikibot.output("Article [[Comuna %s, %s]] does not exists" % (newCommune, county))
             return
+        if redirect and (not redirect.isRedirectPage() or redirect.userName() != "Strainubot"):
+            pywikibot.output(redirect.userName())
+            pywikibot.output("%s already exists" % redirect.title())
+            return
+        title = forwardPage(title)
         redirect = pywikibot.Page(pywikibot.Site(), "Comuna %s, %s" % (oldCommune, county))
         text = u"#redirect [[%s]]" % title.title()
         desc = u"Redirectez %s spre comuna cu care a fost unită prin legea 2/1968 ([[%s]]) #satedisparute1968" % (redirect.title(), title.title())
         pywikibot.output(desc)
         redirect.put(text, desc)
 
-def main(csvfile):
+def main(csvfile, start):
     with open(csvfile, 'r') as csvFile:
         csv_reader = csv.reader(csvFile)
+        count = 0
         for row in csv_reader:
+            count += 1
+            if count < start:
+                continue
             processLine(*row)
 
 if __name__ == u"__main__":
-    main("sate_desfiintate.csv")
-    main("sate_redenumite.csv")
+    main("sate_desfiintate.csv", 545)
+    main("sate_redenumite.csv", 1)
