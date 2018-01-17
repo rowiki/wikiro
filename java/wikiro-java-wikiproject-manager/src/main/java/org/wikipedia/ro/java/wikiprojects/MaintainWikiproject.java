fc5dc4c850d1b9149045d82c436a991f1135cbd5
Andrei Stroe <andreistroe@users.noreply.github.com>
2018-01-17 17:24:41 +0200

    code to flexibly classify pages into wikiprojects

--- java/wikiro-java-wikiproject-manager/src/main/java/org/wikipedia/ro/java/wikiprojects/MaintainWikiproject.java
+++ java/wikiro-java-wikiproject-manager/src/main/java/org/wikipedia/ro/java/wikiprojects/MaintainWikiproject.java
@@ -1,12 +1,18 @@
 package org.wikipedia.ro.java.wikiprojects;
 
+import static org.apache.commons.lang3.StringUtils.removeStart;
+import static org.apache.commons.lang3.StringUtils.split;
+import static org.apache.commons.lang3.StringUtils.startsWith;
+
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.security.auth.login.LoginException;
 
-import static org.apache.commons.lang3.StringUtils.*;
+import org.wikipedia.ro.java.wikiprojects.classify.Classify;
 import org.wikipedia.ro.java.wikiprojects.createcats.CatTreeCreator;
 import org.wikipedia.ro.java.wikiprojects.createproj.WikiprojectCreator;
 import org.wikipedia.ro.java.wikiprojects.stubs.StubClassifier;
@@ -61,6 +67,43 @@ public class MaintainWikiproject {
                 new WikiprojectCreator(args[1], "ro.wikipedia.org", args[2], args[3]).createWikiproject();
             } else {
                 System.err.println("Please specify project name, description and image");
+            }
+        } else if ("classify".equals(args[0])) {
+            if (3 < args.length) {
+                
+            List<String> projects = new ArrayList<String>();
+            List<String> cats = new ArrayList<String>();
+            List<String> recCats = new ArrayList<String>();
+            Map<String, String> projImportance = new LinkedHashMap<String, String>();
+            int depth = 3;
+            for (int i = 2; i < args.length; i++) {
+                String eachArg = args[i];
+                if (startsWith(eachArg, "-project:")) {
+                    projects.add(removeStart(eachArg, "-project:"));
+                }
+                if (startsWith(eachArg, "-cat:")) {
+                    cats.add(removeStart(eachArg, "-cat:"));
+                }
+                if (startsWith(eachArg, "-catr:")) {
+                    recCats.add(removeStart(eachArg, "-catr:"));
+                }
+                for (String eachProjDef : projects) {
+                    String[] projParts = split(eachProjDef, ':');
+                    String importance = projParts.length > 1 ? projParts[1] : "";
+                    String eachProj = projParts[0];
+                    projImportance.put(eachProj, importance);
+                }
+                if (startsWith(eachArg, "-depth:")) {
+                    try {
+                        depth = Integer.parseInt(removeStart(eachArg, "-depth:"));
+                    } catch(NumberFormatException nfe) {
+                        System.err.println("Error reading depth parameter " + eachArg + "; ignoring...");
+                    }
+                }
+            }
+            new Classify("ro.wikipedia.org", projImportance, cats, recCats, depth).classify();
+            } else {
+                System.err.println("Usage: add projects with -project:PROJECT[:IMPORTANCE] and categories with -cat:CATEGORY or -catr:RECURSE_CATEGORY with -depth:MAXDEPTH");
             }
         }
     }
---
+++ java/wikiro-java-wikiproject-manager/src/main/java/org/wikipedia/ro/java/wikiprojects/classify/Classify.java
@@ -0,0 +1,140 @@
+package org.wikipedia.ro.java.wikiprojects.classify;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
+import static org.apache.commons.lang3.StringUtils.startsWithAny;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.security.auth.login.FailedLoginException;
+import javax.security.auth.login.LoginException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.wikibase.Wikibase;
+import org.wikibase.WikibaseException;
+import org.wikibase.WikibasePropertyFactory;
+import org.wikibase.data.Claim;
+import org.wikibase.data.Entity;
+import org.wikibase.data.Item;
+import org.wikibase.data.Property;
+import org.wikipedia.Wiki;
+import org.wikipedia.ro.java.wikiprojects.utils.Credentials;
+import org.wikipedia.ro.java.wikiprojects.utils.WikiprojectsModel;
+import org.wikipedia.ro.java.wikiprojects.utils.WikiprojectsUtils;
+import org.wikipedia.ro.utils.PageUtils;
+
+public class Classify {
+
+    private Map<String, String> projects;
+    private List<String> recursiveCategories;
+    private List<String> categories;
+    private int depth;
+    private String wikiAddress;
+
+    public Classify(String wikiAddress, Map<String, String> projects, List<String> categories,
+        List<String> recursiveCategories, int depth) {
+        super();
+        this.wikiAddress = wikiAddress;
+        this.projects = projects;
+        this.categories = categories;
+        this.recursiveCategories = recursiveCategories;
+        this.depth = depth;
+    }
+
+    public void classify() {
+        Wiki rowiki = new Wiki(wikiAddress);
+        Credentials credentials = WikiprojectsUtils.identifyCredentials();
+        try {
+            rowiki.login(credentials.username, credentials.password);
+            rowiki.setMarkBot(true);
+
+            if (projects.size() == 0) {
+                System.err.println("No projects specified");
+                return;
+            }
+            if (recursiveCategories.size() + categories.size() == 0) {
+                System.err.println("No categories specified");
+                return;
+            }
+
+            Set<String> pagesToRun = new LinkedHashSet<String>();
+            for (String eachCat : categories) {
+                pagesToRun.addAll(Arrays.asList(rowiki.getCategoryMembers(eachCat, Wiki.MAIN_NAMESPACE)));
+            }
+            for (String eachRecCat : recursiveCategories) {
+                pagesToRun.addAll(Arrays.asList(rowiki.getCategoryMembers(eachRecCat, depth, false, Wiki.MAIN_NAMESPACE)));
+            }
+
+            Wikibase dwiki = new Wikibase("www.wikidata.org");
+            for (String eachArticleInCat : pagesToRun) {
+                String eachTalkPageOfArticleInCat = rowiki.getTalkPage(eachArticleInCat);
+                String talkPageText = rowiki.getPageText(eachTalkPageOfArticleInCat);
+                WikiprojectsModel projectModel = WikiprojectsModel.fromTalkPage(talkPageText);
+                if (isBlank(projectModel.getQualClass())) {
+                    if (startsWithAny(eachArticleInCat, "Legislatura", "Lista", "Listă", "Galerie", "Galeria")) {
+                        projectModel.setQualClass("listă");
+                    } else {
+                        int proseSize = PageUtils.getProseSize(rowiki, eachArticleInCat);
+                        String qualClass =
+                            proseSize < 700 ? "ciot" : (proseSize < 2500 ? "start" : (proseSize < 10000 ? "început" : null));
+                        if (null != qualClass) {
+                            projectModel.setQualClass(qualClass);
+                        }
+
+                        Entity wdEntity = dwiki.getWikibaseItemBySiteAndTitle("rowiki", eachArticleInCat);
+                        Map<Property, Set<Claim>> wdClaims = wdEntity.getClaims();
+                        Set<Claim> instanceOfClaims = wdClaims.get(WikibasePropertyFactory.getWikibaseProperty("P31"));
+                        boolean isHuman = false;
+                        if (null != instanceOfClaims) {
+                            for (Claim eachInstanceOfClaim : instanceOfClaims) {
+                                if ("Q5".equals(((Item) eachInstanceOfClaim.getMainsnak().getData()).getEnt().getId())) {
+                                    isHuman = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if (isHuman) {
+                            Set<Claim> deathDateClaims = wdClaims.get(WikibasePropertyFactory.getWikibaseProperty("P570"));
+                            if (null == deathDateClaims || 0 == deathDateClaims.size()) {
+                                projectModel.setLivingPerson(true);
+                            }
+                        }
+                    }
+                }
+
+                for (String eachProj : projects.keySet()) {
+                    if (!projectModel.isInProject(eachProj) || isBlank(projectModel.getImportance(eachProj))) {
+                        projectModel.setImportance(eachProj, projects.get(eachProj));
+                    }
+                }
+
+                String newTalkPageText = projectModel.saveToTalkPage(talkPageText);
+                if (!StringUtils.equals(newTalkPageText, talkPageText)) {
+                    if (!"listă".equals(projectModel.getQualClass())) {
+                        newTalkPageText = newTalkPageText.replaceAll("\\{\\{[Bb]pv\\}\\}\\s*", "");
+                    }
+                    rowiki.edit(eachTalkPageOfArticleInCat, newTalkPageText, "Robot: luat în evidență pentru wikiproiecte");
+                }
+            }
+
+        } catch (FailedLoginException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (LoginException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (WikibaseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            rowiki.logout();
+        }
+    }
+}
