b7d6068bebe946cfca627f2c49be883f182b257f
Andrei Stroe <andreistroe@users.noreply.github.com>
2018-02-16 17:31:01 +0200

    null-safety, more concise

--- java/wikiro-java-wikiproject-manager/src/main/java/org/wikipedia/ro/java/wikiprojects/WikiprojectsHierarchy.java
+++ java/wikiro-java-wikiproject-manager/src/main/java/org/wikipedia/ro/java/wikiprojects/WikiprojectsHierarchy.java
@@ -13,20 +13,6 @@ public class WikiprojectsHierarchy {
 
     private static final Map<String, Wikiproject> WIKIPROJECTS = new HashMap<>();
     static {
-        declareProject("Muzică rock");
-        declareProject("Muzică");
-        declareProject("Transport");
-        declareProject("Aviație");
-        declareProject("Drumuri");
-        declareProject("Trenuri");
-        declareProject("Istorie");
-        declareProject("Istorie militară");
-        declareProject("Țările lumii");
-        declareProject("România");
-        declareProject("Localitățile din România");
-        declareProject("Comune-Sate");
-        declareProject("Orașele României");
-
         declareParent("Muzică", "Muzică rock");
         declareParent("Transport", "Aviație");
         declareParent("Transport", "Drumuri");
@@ -39,14 +25,19 @@ public class WikiprojectsHierarchy {
     }
 
     private static void declareParent(String parent, String child) {
+        declareProject(parent);
+        declareProject(child);
         WIKIPROJECTS.get(child).getParents().add(WIKIPROJECTS.get(parent));
     }
 
     private static void declareProject(String projname) {
-        WIKIPROJECTS.put(projname, new Wikiproject(projname));
+        if (!WIKIPROJECTS.containsKey(projname)) {
+            WIKIPROJECTS.put(projname, new Wikiproject(projname));
+        }
     }
 
     public static boolean isParent(String parentname, String childname) {
-        return WIKIPROJECTS.get(childname).getParents().contains(WIKIPROJECTS.get(parentname));
+        return WIKIPROJECTS.containsKey(childname) && WIKIPROJECTS.containsKey(parentname)
+            && WIKIPROJECTS.get(childname).getParents().contains(WIKIPROJECTS.get(parentname));
     }
 }
