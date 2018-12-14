ca3908f2094752dd8ef9608c8382e65479984ca8
Andrei Stroe <andreistroe@users.noreply.github.com>
2018-12-14 11:05:24 +0200

    separate pass from user; more verbose prompt

--- java/wikiro-java-wikiproject-manager/src/main/java/org/wikipedia/ro/java/wikiprojects/utils/WikiprojectsUtils.java
+++ java/wikiro-java-wikiproject-manager/src/main/java/org/wikipedia/ro/java/wikiprojects/utils/WikiprojectsUtils.java
@@ -1,6 +1,7 @@
 package org.wikipedia.ro.java.wikiprojects.utils;
 
 import static org.apache.commons.lang3.StringUtils.defaultString;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 import java.io.Console;
 
@@ -9,15 +10,16 @@ public class WikiprojectsUtils {
         Credentials credentials = new Credentials();
         
         credentials.username = defaultString(System.getenv("WIKI_WIKIPROJECT_MANAGER_USERNAME"), System.getProperty("WIKI_WIKIPROJECT_MANAGER_USERNAME"));
-        String password = defaultString(System.getenv("WIKI_WIKIPROJECT_MANAGER_PASSWORD"), System.getProperty("WIKI_WIKIPROJECT_MANAGER_PASSWORD"));
 
         Console c = System.console();
-        if (null == credentials.username) {
+        if (isEmpty(credentials.username)) {
             credentials.username = c.readLine("User name: ");
             System.setProperty("WIKI_WIKIPROJECT_MANAGER_USERNAME", credentials.username);
         }
-        if (null == password) {
-            c.printf("Password: ");
+
+        String password = defaultString(System.getenv("WIKI_WIKIPROJECT_MANAGER_PASSWORD"), System.getProperty("WIKI_WIKIPROJECT_MANAGER_PASSWORD"));
+        if (isEmpty(password)) {
+            c.printf("Password for user %s: ", credentials.username);
             credentials.password = c.readPassword();
             System.setProperty("WIKI_WIKIPROJECT_MANAGER_PASSWORD", new String(credentials.password));
         } else {
