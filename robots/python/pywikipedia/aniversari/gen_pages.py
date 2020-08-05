a5aedfda64530de1ebc14d29aee0887300bda1aa
Strainu <github@strainu.ro>
2020-08-05 21:12:49 +0000

    aniversari: Import data from Wikidata
    Import births and deaths from Wikidata for people with articles on ro.wp
    
    Signed-off-by: Strainu <github@strainu.ro>

--- robots/python/pywikipedia/aniversari/gen_pages.py
+++ robots/python/pywikipedia/aniversari/gen_pages.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 
-import copy
 from collections import OrderedDict
+import copy
 import datetime
+from dateutil.relativedelta import relativedelta
+import hashlib
 import math
 import pywikibot
-import sys
+from pywikibot.data import sparql
 import re
+import sys
 import time
-import hashlib
+import urllib.parse
 
 sys.path.append("/home/andrei/pywikibot-core/wikiro/robots/python/pywikipedia")
 import strainu_functions as sf
@@ -27,7 +30,11 @@ sections = [
 	"Decese",
 ]
 
-calendars = [ "Q1985727", "Q1985786" ] 
+all_sections = [i.lower() for i in sections] + \
+               ["note", "premii nobel", "medalia fields", "nedatate",
+                "arte, științe, literatură și filozofie", "legături externe"]
+
+calendars = [ "Q1985727", "Q1985786" ]
 
 events = {}
 
@@ -45,7 +52,7 @@ def get_section_index(page, name):
          #print(section['anchor'])
          if section['anchor'] == name:
              return section['index']
-    return None 
+    return None
 
 ienr = re.compile(r"([1-9]\d{0,3}) (î\.\s?[Hh]r\.?|î\.\s?e\.\s?n\.?)")
 yr = re.compile(r"([1-9]\d{0,3})")
@@ -81,12 +88,12 @@ def _jdn2date(jdn, gregorian):
     day   = math.floor(math.fmod (h,153) / 5) + 1
     month = math.fmod (math.floor(h/153) + 2, 12) + 1
     year  = math.floor(e/1461) - 4716 + math.floor((14 - month) / 12)
-	
+
     #If year is less than 1, subtract one to convert from a zero based date system to the
     #common era system in which the year -1 (1 B.C.E) is followed by year 1 (1 C.E.).
     if year < 1:
         year = year - 1
-	
+
     return (int(year), int(month), int(day))
 
 
@@ -142,7 +149,7 @@ def convert_calendar(date):
     else:
         (y,m,d) = julian_to_gregorian(date.year, date.month, date.day)
         c = calendars[0]
-    newdate = pywikibot.WbTime(year=y, month=m, day=d, 
+    newdate = pywikibot.WbTime(year=y, month=m, day=d,
               calendarmodel="http://www.wikidata.org/entity/" + c)
     #print(newdate)
     return newdate
@@ -195,43 +202,6 @@ def set_event_text(page, day, month, year, text, comment):
     return True
 
 
-def get_line_elements(text, day, month, year, event):
-    global events
-    elem = OrderedDict({})
-    lines = [x for x in text.split("\n") if len(x) and x[0] == '*']
-    for line in lines:
-        l = line.split(':', 1)
-        if len(l) < 2: #no need to parse more if we can't identify names 
-            continue
-        d, name = l
-        hash256 = hashlib.sha256(name.strip().encode('utf-8')).hexdigest()
-        if not year:
-            y = year_to_int(sf.extractLink(d) or d)
-            name = sf.extractLink(name)
-            if y == None or name == None:
-                print(line)
-                print(y)
-                print(name)
-            else:
-                elem[name] = { 'year': y, 'line': line, 
-                               'hash': hash256 }
-        else:
-            d_day, d_month = parse_date(sf.extractLink(d) or d)
-            if month and month != d_month:
-                continue
-            name = sf.extractLink(name)
-            if d_day == None or d_month == None or name == None:
-                print(line)
-                print(year)
-                print(name)
-            else:
-                elem[name] = { 'day': day, 'month': month, 'line': line,
-                               'hash': hash256 }
-            
-        events[hash256] = elem
-    return elem
-
-
 def get_date_line(date, event, entry):
     global events
     # we should already have this data
@@ -240,7 +210,7 @@ def get_date_line(date, event, entry):
         return None
 
     return e[entry]['line']
-    
+
 
 def replace_date_entry(entry, date, event, oldline, newline):
     text = get_event_text(None, date.day, date.month, None, "Full")
@@ -398,7 +368,7 @@ def get_year_line(date, event, entry):
         return None
 
     return e[entry]['line']
-    
+
 
 def replace_year_entry(entry, date, event, oldline, newline):
     text = get_event_text(None, None, None, date.year, "Full")
@@ -473,11 +443,266 @@ def fix_year(entry, olddate, newdate, event):
     pywikibot.output("Fixed " + entry)
     return True
 
-def treat_year(page, year):
-    for month in range(len(months)):
-        treat_month_year(page, year, month + 1)
+def get_wikidata_events(day, month, year, event):
+    """Query Wikidata for all persons which had the `event` happen in the given
+       date (e.g. born in 2017)
 
-def treat_month_year(page, year, month):
+       @param day: The day we're working on or None
+       @param month: The month we're working on or None
+       @param year: The year we're working on or None
+       @param event: The event we're working on
+       @param_type event: string
+
+       @rtype dictionary
+    """
+
+    if event == "Nașteri":
+        fflt = "born"
+        sflt = "died"
+        fid = "P569"
+        sid = "P570"
+        optional = "OPTIONAL"
+        other_prefix = "d"
+    elif event == "Decese":
+        fflt = "died"
+        sflt = "born"
+        fid = "P570"
+        sid = "P569"
+        optional = ""
+        other_prefix = "n"
+    else:
+        return
+
+    if not year:
+        return
+    stime = datetime.datetime.strptime("{}-{}-{}".format(year, month or 1, day or 1), '%Y-%m-%d')
+    if day:
+        tdiff = relativedelta(days=+1)
+    elif month:
+        tdiff = relativedelta(months=+1)
+    else:
+        tdiff = relativedelta(years=+1)
+    etime = stime + tdiff
+
+    query = """select ?item ?itemLabel ?itemDescription ?born ?bornP ?died ?diedP ?article where {
+    ?item wdt:P31 wd:Q5;
+          p:%s/psv:%s [
+                wikibase:timePrecision ?%sP ;
+                wikibase:timePrecision "11"^^xsd:integer ;
+                wikibase:timeValue ?%s ;
+              ] .
+    %s {
+    ?item p:%s/psv:%s [
+                wikibase:timePrecision ?%sP ;
+                wikibase:timePrecision "11"^^xsd:integer ;
+                wikibase:timeValue ?%s ;
+              ] .
+    }
+    filter (?%s >= "%sZ"^^xsd:dateTime && ?%s < "%sZ"^^xsd:dateTime)
+
+    ?article schema:about ?item .
+    ?article schema:isPartOf <https://ro.wikipedia.org/> . #Targeting Wikipedia language where subjects has no article.
+    SERVICE wikibase:label {
+       bd:serviceParam wikibase:language "ro"
+    }
+} LIMIT 100""" % (fid, fid, fflt, fflt,
+                 optional, sid, sid, sflt, sflt,
+                 fflt, stime.isoformat(), fflt, etime.isoformat())
+
+    query_object = sparql.SparqlQuery(max_retries=0)
+    try:
+        data = query_object.query(query)
+    except:
+        data = None
+    if not data:
+        return
+
+    ret = {}
+    results = data['results']['bindings']
+
+    for result in results:
+        if int(result['bornP']['value']) < 11:
+            continue
+        if 'diedP' in result and int(result['diedP']['value']) < 11:
+            continue
+
+        if 'itemDescription' not in result:
+            continue
+
+        name = result['itemLabel']['value']
+        if name in ret:
+            # multiple values in a field, we don't bother with that
+            del ret[name]
+            continue
+
+        if sflt in result:
+            s = datetime.datetime.strptime(result[sflt]['value'],'%Y-%m-%dT%H:%M:%SZ')
+            other_text = " ({prefix}. [[{y}]])".format(prefix=other_prefix, y=s.year)
+        else:
+            other_text = ""
+        d = datetime.datetime.strptime(result[fflt]['value'],'%Y-%m-%dT%H:%M:%SZ')
+        desc = result['itemDescription']['value']
+        desc = desc[0].lower() + desc[1:]
+        article = urllib.parse.unquote(result['article']['value'][len("https://ro.wikipedia.org/wiki/"):]).replace('_',' ')
+        if article != name:
+            line = "[[{article}|{name}]], {desc}{other}".format(article=article, name=name, desc=desc, other=other_text)
+        else:
+            line = "[[{article}]], {desc}{other}".format(article=article, desc=desc, other=other_text)
+        hash256 = hashlib.sha256(line.strip().encode('utf-8')).hexdigest()
+
+        ret[name] = { 'day': d.day, 'month': months[d.month-1], 'year': d.year,
+                      'event': event, 'line': line, 'hash': hash256 }
+
+    print(ret)
+    return ret
+
+def split_page_in_sections(page):
+    """Split a page in a list of sections
+
+       @param page	The page to split
+       @returns		The list of sections including the section headers
+    """
+    ptext = page.get()
+    slist = re.split("\={2,}\s*([^\=]+)\s*\={2,}\n", ptext, flags=re.I)
+
+    return slist
+
+def sort_line_dict(d):
+    """Helper function for sorting line entries in aniversaries"""
+    for month_d in range(len(months)):
+        if months[month_d] == d['month']:
+            break
+
+    return(d['year'], month_d+1, d['day'])
+
+def fill_year_from_wikidata(page, year):
+    """Add events from Wikidata to year page
+
+       @param page: The year page
+       @param year: The year as integer
+    """
+    for event in sections[1:]:
+        for month in range(1,13):
+            text = page.get()
+            slist = split_page_in_sections(page)
+            wdlines = get_wikidata_events(None, month, year, event)
+            if wdlines == None or not len(wdlines):
+                print("No data found in Wikidata for section {} in {}".format(event, year))
+                continue
+
+            old_text = None
+
+            for slist_index in range(len(slist)):
+                m = sf.stripLink(slist[slist_index].strip()) or slist[slist_index].strip()
+                if m.lower() == event.lower():
+                    for month_index in range(slist_index+1, len(slist)):
+                        n = sf.stripLink(slist[month_index].strip()) or slist[month_index].strip()
+                        if n.lower() == months[month-1].lower():
+                            # Found month section
+                            old_text = slist[month_index + 1]
+                            break
+                        elif slist[month_index].strip().lower() in all_sections:
+                            old_text = "".join(slist[slist_index+1:month_index])
+                            break
+                    break
+
+            if not old_text:
+                print("No section found for {} {}-{}".format(event, year, month))
+                continue
+
+            wlines = get_line_elements(old_text, None, None, year, event)
+
+            for line in wdlines:
+                if line in wlines:
+                    continue
+                if wdlines[line]['month'] != months[month-1]:
+                    continue
+                wlines[line] = wdlines[line]
+
+            wlines = sorted(wlines.values(), key=sort_line_dict)
+            new_text = old_text
+            prev_line = None
+            for line in wlines:
+                if new_text.find(line['line']) == -1:
+                    line_text = "* [[{day} {month}]]: {line}".format(day=line['day'], month=line['month'], line=line['line'])
+                    if prev_line:
+                        prev_line_text = "* [[{day} {month}]]: {line}".format(day=prev_line['day'], month=prev_line['month'], line=prev_line['line'])
+                        new_text = new_text.replace(prev_line_text, prev_line_text + "\n" + line_text)
+                    else:
+                        new_text = line_text + "\n" + new_text
+                prev_line = line
+
+            if new_text == old_text:
+                print("No new data for {} {}-{}".format(event, year, month))
+                continue
+
+            text = text.replace(old_text, new_text)
+
+            #pywikibot.showDiff(text, text2)
+            page.put(text, "Actualizare {} cu date de la Wikidata".format(event))
+
+def get_line_elements(text, day, month, year, event):
+    """Extract all events from `text` assuming they're in standardized format
+
+       @param text: The text to work on
+       @param day: The day we're working on or None
+       @param month: The month we're working on or None
+       @param year: The year we're working on or None
+       @param event: The event we're working on or None
+    """
+    global events
+    elem = OrderedDict({})
+    lines = [x for x in text.split("\n") if len(x) and x[0] == '*']
+    for line in lines:
+        l = line.split(':', 1)
+        if len(l) < 2: #no need to parse more if we can't identify names
+            continue
+        d, contents = l
+        hash256 = hashlib.sha256(contents.strip().encode('utf-8')).hexdigest()
+        if not year:
+            y = year_to_int(sf.extractLink(d) or d)
+            name = sf.extractLink(contents)
+            if y == None or name == None:
+                print(line)
+                print(y)
+                print(name)
+            else:
+                elem[name] = { 'day': day, 'month': month, 'year': y,
+                               'event': event, 'line': contents.strip(), 'hash': hash256 }
+        else:
+            d_day, d_month = parse_date(sf.extractLink(d) or d)
+            if month and month != d_month:
+                continue
+            name = sf.extractLink(contents)
+            if d_day == None or d_month == None or name == None:
+                print(line)
+                print(year)
+                print(name)
+            else:
+                elem[name] = { 'day': d_day, 'month': d_month, 'year': year,
+                               'event': event, 'line': contents.strip(), 'hash': hash256 }
+
+        events[hash256] = elem
+    return elem
+
+
+def split_year_data(page, year):
+    """Extract data from year page for montly pages
+
+       @param page: The year page object
+       @param year: The numerical value for the year
+       @rtype: void
+       """
+    for month in range(len(months)):
+        split_year_to_month(page, year, month + 1)
+
+def split_year_to_month(page, year, month):
+    """Construct the monthly page from the data extracted from the year page
+
+       @param page: The year page object
+       @param year: The numerical value for the year
+       @rtype: void
+    """
     publish = False
     try:
         month_s = months[month-1]
@@ -488,12 +713,12 @@ def treat_month_year(page, year, month):
         print("Cannot get month page", e)
         #return
     x = datetime.datetime(year, month, 1)
-    text = "{{{{Antet an|{year}}}}}\n{{{{Calendar|{year}|{month_d}}}}}\n'''{month_s} {year}''' a fost {ordinal} lună a anului și a început într-o zi de {weekday}.\n".format(
-            year=year, month_d=month, ordinal=ordinal[month-1], month_s=month_s, weekday=week[x.weekday()])
-    ptext = page.get()
-    slist = re.split("\={2,}\s*([^\=]+)\s*\={2,}\n", ptext, flags=re.I)
-
-    all_sections = [i.lower() for i in sections] + ["note", "premii nobel", "arte, științe, literatură și filozofie", "nedatate", "legături externe", "medalia fields"]
+    text = """{{{{Articol generat automat|[[{year}]]}}}}{{{{Antet an|{year}}}}}
+{{{{Calendar|{year}|{month_d}}}}}
+'''{month_s} {year}''' a fost {ordinal} lună a anului și a început într-o zi de {weekday}.
+""".format(year=year, month_d=month, ordinal=ordinal[month-1],
+           month_s=month_s.capitalize(), weekday=week[x.weekday()])
+    slist = split_page_in_sections(page)
 
     for section in sections:
         for slist_index in range(len(slist)):
@@ -511,7 +736,7 @@ def treat_month_year(page, year, month):
                     elif slist[month_index].strip().lower() in all_sections:
                         # No section for month, try manual parsing
                         print("Unexpected section %s" % slist[month_index].strip())
-                        people = treat_month_year_manual(page, year, month_s, slist, slist_index, month_index)
+                        people = split_year_to_month_manual(page, year, month_s, slist, slist_index, month_index)
                         if len(people):
                             text += "\n== " + section + " ==\n"
                             publish = True
@@ -532,31 +757,37 @@ def treat_month_year(page, year, month):
         text += "\n== Note ==\n<references />\n"
         text += "[[Categorie:%d|%s]]\n" % (year, month_s)
         text += "[[Categorie:%s|%d]]\n" % (month_s, year)
-        if len(text) < 1000:
-            #don't publish stubs
-            return
         mpage.put(text, "Actualizare articol pe baza articolului [[%s]]" % page.title())
 
-def treat_month_year_manual(page, year, month, ev_list, start_index, end_index):
+def split_year_to_month_manual(page, year, month, ev_list, start_index, end_index):
     people = OrderedDict({})
     for index in range(start_index, end_index):
         people.update(get_line_elements(ev_list[index], None, month, year, ev_list[start_index].strip().lower()))
     print(people)
     return people
 
-def main():
-    for year in range(2020, 1+time.localtime().tm_year):
-        #for suffix in ["", " î.Hr."]:
+def use_wikidata():
+    for year in range(1980, 1+time.localtime().tm_year):
         page = pywikibot.Page(pywikibot.getSite(),  "%d" % (year))
         if not page.exists():
             continue
         if page.isRedirectPage():
             page = page.getRedirectTarget()
-        #if suffix != "":
-        #     year = -year
-        treat_year(page, year)
+
+        fill_year_from_wikidata(page, year)
+
+def split_years():
+    for year in range(1980, 1+time.localtime().tm_year):
+        page = pywikibot.Page(pywikibot.getSite(),  "%d" % (year))
+        if not page.exists():
+            continue
+        if page.isRedirectPage():
+            page = page.getRedirectTarget()
+
+        split_year_data(page, year)
 
 if __name__ == "__main__":
     #import cProfile
     #cProfile.run('main()', 'profiling_aniversari.txt')
-    main()
+    #split_years()
+    use_wikidata()
