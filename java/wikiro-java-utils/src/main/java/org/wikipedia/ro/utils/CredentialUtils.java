776823010cc3de2ebafa302fe5bd2cea506bf98b
Andrei Stroe <andreistroe@users.noreply.github.com>
2021-03-07 11:01:38 +0200

    remove project-specific labels

--- java/wikiro-java-utils/src/main/java/org/wikipedia/ro/utils/CredentialUtils.java
+++ java/wikiro-java-utils/src/main/java/org/wikipedia/ro/utils/CredentialUtils.java
@@ -9,19 +9,19 @@ public class CredentialUtils {
     public static Credentials identifyCredentials() {
         Credentials credentials = new Credentials();
         
-        credentials.username = defaultString(System.getenv("WIKI_WIKIPROJECT_MANAGER_USERNAME"), System.getProperty("WIKI_WIKIPROJECT_MANAGER_USERNAME"));
+        credentials.username = defaultString(System.getenv("WIKI_USERNAME"), System.getProperty("WIKI_USERNAME"));
 
         Console c = System.console();
         if (isEmpty(credentials.username)) {
             credentials.username = c.readLine("User name: ");
-            System.setProperty("WIKI_WIKIPROJECT_MANAGER_USERNAME", credentials.username);
+            System.setProperty("WIKI_USERNAME", credentials.username);
         }
 
-        String password = defaultString(System.getenv("WIKI_WIKIPROJECT_MANAGER_PASSWORD"), System.getProperty("WIKI_WIKIPROJECT_MANAGER_PASSWORD"));
+        String password = defaultString(System.getenv("WIKI_PASSWORD"), System.getProperty("WIKI_PASSWORD"));
         if (isEmpty(password)) {
             c.printf("Password for user %s: ", credentials.username);
             credentials.password = c.readPassword();
-            System.setProperty("WIKI_WIKIPROJECT_MANAGER_PASSWORD", new String(credentials.password));
+            System.setProperty("WIKI_PASSWORD", new String(credentials.password));
         } else {
             credentials.password = password.toCharArray();
         }
