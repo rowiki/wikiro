d0cf5fab4920eb7f8c711eed1f77334c107e1075
Andrei Stroe <andreistroe@users.noreply.github.com>
2021-06-22 09:22:16 +0300

    bring robot up-to-date with pywikibot fwk

--- robots/python/pywikipedia/paperboy.py
+++ robots/python/pywikipedia/paperboy.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-import pywikibot,catlib
+import pywikibot
 from pywikibot import page,pagegenerators
 
-site = pywikibot.getSite()
-cat = catlib.Category(site, u"Categorie:Cereri de deblocare")
+site = pywikibot.Site()
+cat = pywikibot.Category(site, u"Categorie:Cereri de deblocare")
 gen = pagegenerators.CategorizedPageGenerator(cat)
 
 #get all outstanding requests
 reqs = []
 for eachpage in gen:
-  reqs.append(eachpage.titleWithoutNamespace())
+  reqs.append(eachpage.title(withNamespace=False))
  
 #if there are any
 if len(reqs) > 0:
@@ -17,8 +17,8 @@ if len(reqs) > 0:
   subscriberspage = page.Page(site, u"Utilizator:Andrebot/Abonați newsletter sysop")
   subscriberlinks = subscriberspage.linkedPages()
   for subscribername in subscriberlinks:
-    pywikibot.output(u"Subscriber: " + subscribername.titleWithoutNamespace())
-    subscribertalkpage = page.Page(site, u"Discuție Utilizator:" + subscribername.titleWithoutNamespace())
+    pywikibot.output(u"Subscriber: " + subscribername.title(withNamespace=False))
+    subscribertalkpage = page.Page(site, u"Discuție Utilizator:" + subscribername.title(withNamespace=False))
     #grab the subscriber's page
     tptext = subscribertalkpage.get()
     
@@ -28,9 +28,9 @@ if len(reqs) > 0:
       if tptext.find(u"Andrebot.Newsletter.Notificat:" + req) < 0:
         newrequests.append(req)
       else:
-        pywikibot.output(subscribername.titleWithoutNamespace() + u" already knows about " + req)
+        pywikibot.output(subscribername.title(withNamespace=false) + u" already knows about " + req)
     if len(newrequests) > 0:
-      pywikibot.output(u"Notifying " + subscribername.titleWithoutNamespace() + " about " + repr(newrequests))
+      pywikibot.output(u"Notifying " + subscribername.title(withNamespace=False) + " about " + repr(newrequests))
       tptext = tptext + u"\n" + u"== Cereri de deblocare ==\n"
       if len(newrequests) == 1:
         tptext = tptext + u"Utilizatorul [[Discuție Utilizator:" + newrequests[0] + u"|" + newrequests[0] + u"]] a cerut deblocarea."
@@ -46,4 +46,4 @@ if len(reqs) > 0:
     
       subscribertalkpage.put(tptext, minorEdit=False, comment = u"Robot: notificare cereri de deblocare")
     else:
-      pywikibot.output(subscribername.titleWithoutNamespace() + u" already informed")
+      pywikibot.output(subscribername.title(withNamespace=False) + u" already informed")
