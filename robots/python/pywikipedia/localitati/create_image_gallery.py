535f28372615adaba82d8ccc1a9bfb89a7554b35
Strainu <github@strainu.ro>
2021-10-28 19:47:08 +0000

    feat: Uploader for AIRM monuments.
    This is a very custom script based on a certain folder structure and
    the tables from [[:c:User:Strainu/AIRM]]. This version is specifically for
    the Collections subpage.
    
    Signed-off-by: Strainu <github@strainu.ro>

--- robots/python/pywikipedia/localitati/create_image_gallery.py
+++ robots/python/pywikipedia/localitati/create_image_gallery.py
@@ -17,6 +17,7 @@ import otherconfig as config
 from geo import mapillary
 
 gallery = []
+table = []
 stats = {}
 candidates = {}
 fix = False
@@ -90,13 +91,28 @@ def dump_mapillary_data():
 def treat_sparql(dic):
     global gallery
     global stats
+    global table
     try:
+        table_template = """|- class="{css}"
+| {siruta} 
+| class="uat-list link" | [[{name}]] 
+| class="uat-list image" | [[File:{file}|faracadru|200px]]<br/>{{{{Plain link|1={upload}|2=Încarcă imagine}}}}"""
         name = dic['itemLabel']
         img = dic['image']
         upperImg = dic['upperImage']
         siruta = dic['siruta']
         linkLabel = name + " (" + siruta + ")"
         county = dic.get('countyLabel')
+        commons = dic.get('commons')
+        #print(dic)
+        if dic.get('mayor') != None:
+            css = "uat-list uat"
+        else:
+            css = "uat-list settlement"
+        if commons:
+            cats = commons.replace(' ','_')
+        else:
+            cats = ""
         altLink = u'Județul ' + county
         if county not in stats:
             stats[county] = {
@@ -107,15 +123,28 @@ def treat_sparql(dic):
             }
         stats[county]['total'] += 1
         stats['total']['total'] += 1
+        upload_link = '//commons.wikimedia.org/w/index.php?title=Special:UploadWizard&campaign=wlro&uselang=ro&id=' + siruta + '&categories=' + cats
         if img:
             imgl = pywikibot.Link('File:' + img[img.rfind('/')+1:])
             gallery.append(imgl.title + '|<div style="background:lightgreen">[[' + (dic.get('page_title') or altLink) + '|' + linkLabel + ']]</div>')
+            table.append(
+	        table_template.format(css=css,
+                                      siruta=siruta,
+                                      name=(dic.get('page_title') or name),
+                                      file=imgl.title,
+                                      upload=upload_link))
             stats[county]['wikidata'] += 1
             stats['total']['wikidata'] += 1
             return
         if upperImg:
             imgl = pywikibot.Link('File:' + upperImg[upperImg.rfind('/')+1:])
             gallery.append(imgl.title + '|<div style="background:lightgreen">[[' + (dic.get('page_title') or altLink) + '|' + linkLabel + ']]</div>')
+            table.append(
+	        table_template.format(css=css,
+                                      siruta=siruta,
+                                      name=(dic.get('page_title') or name),
+                                      file=imgl.title,
+                                      upload=upload_link))
             stats[county]['wikidata'] += 1
             stats['total']['wikidata'] += 1
             return
@@ -135,11 +164,23 @@ def treat_sparql(dic):
                       " distrikto " not in pi.title() and \
                       " jud " not in pi.title():
                 gallery.append(pi.title() + '|<div style="background:yellow">[[' + rp.title() + '|' + name + " (" + siruta + ")]]</div>")
+                table.append(
+	            table_template.format(css=css,
+                                          siruta=siruta,
+                                          name=(dic.get('page_title') or name),
+                                          file=pi.title().replace('Fișier:',''),
+                                          upload=upload_link))
                 stats[county]['local'] += 1
                 stats['total']['local'] += 1
                 fix_local_image(pi, rp.data_item())
                 return
         gallery.append(u'File:Replace this image - temple.JPG|<div style="background:red">[[' + (dic.get('page_title') or altLink) + '|' + linkLabel + ']]</div>')
+        table.append(
+	        table_template.format(css=css,
+                                      siruta=siruta,
+                                      name=(dic.get('page_title') or name),
+                                      file="Replace this image - temple.JPG",
+                                      upload=upload_link))
         stats[county]['missing'] += 1
         stats['total']['missing'] += 1
     except Exception as e:
@@ -147,8 +188,21 @@ def treat_sparql(dic):
         print(e)
         raise
 
+def dump_table(table):
+    text = """<templatestyles src="Wikipedia:Wiki Loves România/styles.css" />
+Pentru a încărca o imagine, faceți clic pe legătura de sub imagine  și veți fi dus la asistentul de încărcare de unde puteți încărca noi imagini cu localitatea respectivă. Puteți încărca imagini chiar dacă există deja o imagine pentru localitatea respectivă.
+    
+{| class="wikitable sortable center"
+! Cod SIRUTA
+! Nume
+! class="unsortable" | Imagine
+"""
+    for line in table:
+        text += line + "\n"
+    text += "|}"
+    return text
 
-def dump_text(gallery):
+def dump_gallery(gallery):
     #art = pywikibot.Page(pywikibot.Site('ro', 'wikipedia'), 'Utilizator:Strainu/wlro')
     text = "<gallery>\n"
     for img in gallery:
@@ -161,7 +215,7 @@ def dump_text(gallery):
 
 def add_text(where, t, overwrite=False):
     try:
-        art = pywikibot.Page(pywikibot.Site('ro', 'wikipedia'), 'Utilizator:Strainu/wlro/' + where)
+        art = pywikibot.Page(pywikibot.Site('ro', 'wikipedia'), 'Wikipedia:Wiki Loves România/' + where)
         print(art)
         text = ""
         if art.exists() and not overwrite:
@@ -224,6 +278,7 @@ def main():
     user.mylang = 'wikidata'
     user.family = 'wikidata'
     global gallery
+    global table
     gallery = []
     main_text = """{| class=\"wikitable sortable\"
 ! Județ
@@ -238,24 +293,26 @@ def main():
     repo = pywikibot.Site().data_repository()
     dependencies = {'endpoint': None, 'entity_url': None, 'repo': repo}
     query_object = sparql.SparqlQuery(**dependencies)
-    query = """SELECT ?item ?itemLabel ?siruta ?image ?upperImage ?page_title ?countyLabel ?coord
-WHERE
-{
-  ?item wdt:P843 ?siruta.
-  ?item wdt:P131* ?county.
-  ?county wdt:P31 wd:Q1776764.
-  OPTIONAL { ?item wdt:P625 ?coord. }
-  OPTIONAL { ?item wdt:P18 ?image. }
-  OPTIONAL {
-    { ?item wdt:P31 wd:Q34842263. } UNION {?item wdt:P31 wd:Q34842776.}
-    ?item wdt:P1376 ?upper.
-    ?upper wdt:P18 ?upperImage.
-  }
-  OPTIONAL {?article    schema:about ?item ;
-                        schema:isPartOf <https://ro.wikipedia.org/>;  schema:name ?page_title  . }
-  SERVICE wikibase:label { bd:serviceParam wikibase:language 'ro'. }
-}
-ORDER BY ?countyLabel ?itemLabel ?siruta"""
+    query = """SELECT ?item ?itemLabel ?mayor ?siruta ?image ?upperImage ?page_title ?countyLabel ?coord ?commons
+    WHERE
+    {
+      ?item wdt:P843 ?siruta.
+      ?item wdt:P131* ?county.
+      ?county wdt:P31 wd:Q1776764.
+      #OPTIONAL { ?item wdt:P625 ?coord. }
+      OPTIONAL { ?item wdt:P18 ?image. }
+      OPTIONAL { ?item wdt:P373 ?commons. }
+      OPTIONAL { ?item wdt:P6 ?mayor. }
+      #OPTIONAL {
+      #  { ?item wdt:P31 wd:Q34842263. } UNION {?item wdt:P31 wd:Q34842776.}
+      #  ?item wdt:P1376 ?upper.
+      #  ?upper wdt:P18 ?upperImage.
+      #}
+      OPTIONAL {?article    schema:about ?item ;
+                            schema:isPartOf <https://ro.wikipedia.org/>;  schema:name ?page_title  . }
+      SERVICE wikibase:label { bd:serviceParam wikibase:language 'ro'. }
+   }
+   ORDER BY ?countyLabel ?siruta ?itemLabel"""
     data = query_object.select(query)
     if not data:
         return
@@ -280,16 +337,19 @@ ORDER BY ?countyLabel ?itemLabel ?siruta"""
             if result['countyLabel'] != last_county:
                 if last_county:
                     dump_mapillary_data()
-                    add_text(last_county, dump_text(gallery), True)
+                    #add_text(last_county, dump_gallery(gallery), True)
+                    add_text(last_county, dump_table(table), True)
                     main_text += generate_stats(last_county)
                 gallery = []
+                table = []
                 last_county = result['countyLabel']
             treat_sparql(result)
-        add_text(last_county, dump_text(gallery), True)
+        #add_text(last_county, dump_gallery(gallery), True)
+        add_text(last_county, dump_table(table), True)
         main_text += generate_stats(last_county)
         main_text += generate_stats('total') + "\n|}"
-        art = pywikibot.Page(pywikibot.Site('ro', 'wikipedia'), 'Utilizator:Strainu/wlro')
-        art.put(main_text, "Actualizare statistici")
+        art = pywikibot.Page(pywikibot.Site('ro', 'wikipedia'), 'Wikipedia:Wiki Loves România#Situația_la_zi')
+        #art.put(main_text, "Actualizare statistici")
         dump_mapillary_data()
     except Exception as e:
         print(e)
