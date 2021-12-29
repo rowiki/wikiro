ab27945fdc17e12e35d378cbad961ba216bb1908
Andrei Stroe <and.rei.s.tr.o.e+github@gmail.com>
2021-12-29 04:02:10 +0200

    updating paperboy to latest pywikipedia framework

--- robots/python/pywikipedia/paperboy.py
+++ robots/python/pywikipedia/paperboy.py
@@ -9,7 +9,7 @@ gen = pagegenerators.CategorizedPageGenerator(cat)
 #get all outstanding requests
 reqs = []
 for eachpage in gen:
-  reqs.append(eachpage.title(withNamespace=False))
+  reqs.append(eachpage.title(with_ns=False))
  
 #if there are any
 if len(reqs) > 0:
@@ -17,8 +17,8 @@ if len(reqs) > 0:
   subscriberspage = page.Page(site, u"Utilizator:Andrebot/Abonați newsletter sysop")
   subscriberlinks = subscriberspage.linkedPages()
   for subscribername in subscriberlinks:
-    pywikibot.output(u"Subscriber: " + subscribername.title(withNamespace=False))
-    subscribertalkpage = page.Page(site, u"Discuție Utilizator:" + subscribername.title(withNamespace=False))
+    pywikibot.output(u"Subscriber: " + subscribername.title(with_ns=False))
+    subscribertalkpage = page.Page(site, u"Discuție Utilizator:" + subscribername.title(with_ns=False))
     #grab the subscriber's page
     tptext = subscribertalkpage.get()
     
@@ -28,9 +28,9 @@ if len(reqs) > 0:
       if tptext.find(u"Andrebot.Newsletter.Notificat:" + req) < 0:
         newrequests.append(req)
       else:
-        pywikibot.output(subscribername.title(withNamespace=false) + u" already knows about " + req)
+        pywikibot.output(subscribername.title(with_ns=False) + u" already knows about " + req)
     if len(newrequests) > 0:
-      pywikibot.output(u"Notifying " + subscribername.title(withNamespace=False) + " about " + repr(newrequests))
+      pywikibot.output(u"Notifying " + subscribername.title(with_ns=False) + " about " + repr(newrequests))
       tptext = tptext + u"\n" + u"== Cereri de deblocare ==\n"
       if len(newrequests) == 1:
         tptext = tptext + u"Utilizatorul [[Discuție Utilizator:" + newrequests[0] + u"|" + newrequests[0] + u"]] a cerut deblocarea."
@@ -44,6 +44,6 @@ if len(reqs) > 0:
       tptext = tptext + u"Puteți șterge acest mesaj după rezolvarea cererilor. Dacă o faceți înainte de rezolvarea cererilor, veți primi notificarea din nou până când cererea va fi rezolvată.</small>"
       tptext = tptext + u"&nbsp;&ndash;~~~~"
     
-      subscribertalkpage.put(tptext, minorEdit=False, comment = u"Robot: notificare cereri de deblocare")
+      subscribertalkpage.put(tptext, minor=False, summary = u"Robot: notificare cereri de deblocare")
     else:
-      pywikibot.output(subscribername.title(withNamespace=False) + u" already informed")
+      pywikibot.output(subscribername.title(with_ns=False) + u" already informed")
