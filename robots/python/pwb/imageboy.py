3ceb5c7af0dcfbf4ae5e01dd37da0590c3314a9c
Strainu <github@strainu.ro>
2022-04-20 21:59:49 +0000

    imageboy: Update to the latest PWB version
    Signed-off-by: Strainu <github@strainu.ro>

--- robots/python/pwb/imageboy.py
+++ robots/python/pwb/imageboy.py
@@ -1,35 +1,36 @@
 # -*- coding: utf-8 -*-
-import wikipedia,catlib,pagegenerators
+import pywikibot
+from pywikibot import pagegenerators
 
-site = wikipedia.getSite()
-cat = catlib.Category(site, u"Categorie:Răspunsuri imagini")
+site = pywikibot.Site()
+cat = pywikibot.Category(site, u"Categorie:Răspunsuri imagini")
 gen = pagegenerators.CategorizedPageGenerator(cat)
 
 #get all outstanding requests
 reqs = []
 for page in gen:
-  reqs.append(page.titleWithoutNamespace())
- 
+  reqs.append(page.title(with_ns=False))
+
 #if there are any
 if len(reqs) > 0:
   #get all subscribers
-  subscriberspage = wikipedia.Page(site, u"Utilizator:Andrebot/Abonați newsletter sysop")
+  subscriberspage = pywikibot.Page(site, u"Utilizator:Andrebot/Abonați newsletter sysop")
   subscriberlinks = subscriberspage.linkedPages()
   for subscribername in subscriberlinks:
-    wikipedia.output(u"Subscriber: " + subscribername.titleWithoutNamespace())
-    subscribertalkpage = wikipedia.Page(site, u"Discuție Utilizator:" + subscribername.titleWithoutNamespace())
+    pywikibot.output(u"Subscriber: " + subscribername.title(with_ns=False))
+    subscribertalkpage = pywikibot.Page(site, u"Discuție Utilizator:" + subscribername.title(with_ns=False))
     #grab the subscriber's page
     tptext = subscribertalkpage.get()
-    
+
     # check which users he doesn't know of
     newrequests = []
     for req in reqs:
       if tptext.find(u"Strainubot.Newsletter.Notificat:" + req) < 0:
         newrequests.append(req)
       else:
-        wikipedia.output(subscribername.titleWithoutNamespace() + u" already knows about " + req)
+        pywikibot.output(subscribername.title(with_ns=False) + u" already knows about " + req)
     if len(newrequests) > 0:
-        wikipedia.output(u"Notifying " + subscribername.titleWithoutNamespace() + " about " + repr(newrequests))
+        pywikibot.output(u"Notifying " + subscribername.title(with_ns=False) + " about " + repr(newrequests))
         tptext = tptext + u"\n" + u"== Răspunsuri avertizări imagini ==\n"
         if len(newrequests) == 1:
             tptext = tptext + u"Utilizatorul [[Discuție Utilizator:" + newrequests[0] + u"|" + newrequests[0] + u"]] a lăsat un răspuns la un mesaj de avertizare despre o imagine."
@@ -38,14 +39,14 @@ if len(reqs) > 0:
             tptext = tptext + u"Utilizatorii (" + str(len(reqs)) + u" la număr):"
         for req in newrequests:
             tptext = tptext + u"\n* [[Discuție Utilizator:" + req + u"|" + req + u"]] <!--Strainubot.Newsletter.Notificat:" + req + u"-->"
-        tptext = tptext + u"\nau lăsat un răspuns la un mesaj de avertizare despre o imagine."
+            tptext = tptext + u"\nau lăsat un răspuns la un mesaj de avertizare despre o imagine."
         tptext = tptext + u" Încărcarea imaginilor și drepturile de autor sunt subiecte sensibile. Este important ca toate neclaritățile cu privire la avertizări să fie lămurite pentru a evita apariția unor nemulțumiri ce pot fi evitate."
         tptext = tptext + u"\n\n<small>Notă: Ați primit acest mesaj automat deoarece sunteți înscris(ă) pe lista de la pagina [[Utilizator:Andrebot/Abonați newsletter sysop]]. Pentru a vă dezabona, ștergeți-vă numele din acea listă."
         tptext = tptext + u"Puteți șterge acest mesaj după rezolvarea cererilor. Dacă o faceți înainte de rezolvarea cererilor, veți primi notificarea din nou până când cererea va fi rezolvată.</small>"
         tptext = tptext + u"&nbsp;&ndash;~~~~"
-    
-        subscribertalkpage.put(tptext, minorEdit=False, comment = u"Robot: notificare răspunsuri la avertizări pentru imagini")
+
+        subscribertalkpage.put(tptext, minorEdit=False, summary = u"Robot: notificare răspunsuri la avertizări pentru imagini")
     else:
-        wikipedia.output(subscribername.titleWithoutNamespace() + u" already informed")
+        pywikibot.output(subscribername.title(with_ns=False) + u" already informed")
 else:
-    wikipedia.output(u"Nothing to do!")
+   pywikibot.output(u"Nothing to do!")
