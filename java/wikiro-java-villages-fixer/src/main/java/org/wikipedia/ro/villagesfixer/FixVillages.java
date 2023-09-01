688fb5d14c716a48277b8da8f0135561e88699de
Andrei Stroe <andreistroe@users.noreply.github.com>
2023-09-01 17:04:43 +0300

    go only until the first div

--- java/wikiro-java-villages-fixer/src/main/java/org/wikipedia/ro/villagesfixer/FixVillages.java
+++ java/wikiro-java-villages-fixer/src/main/java/org/wikipedia/ro/villagesfixer/FixVillages.java
@@ -51,7 +51,6 @@ import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
-import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -145,7 +144,7 @@ public class FixVillages {
     private static Pattern ifPattern = Pattern.compile("\\{\\{\\s*#if");
 
     private static Pattern demographyChartsPattern =
-        Pattern.compile("<div style=\"float:left\">\\{\\{Pie chart(.*)</div>", Pattern.DOTALL);
+        Pattern.compile("<div style=\"float:left\">\\{\\{Pie chart(.*?)</div>", Pattern.DOTALL);
     private static Pattern demographyTextPattern = Pattern.compile(
         "Conform \\[\\[Recensământul (.*)<ref name=\"insse_20\\d1_rel\">.*</ref>");
 
