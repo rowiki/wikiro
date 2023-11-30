839e8260060496f3af34e73950a8bb8a8166ad2c
Strainu <github@strainu.ro>
2023-11-30 16:18:30 +0000

    feat: Add support for saving historic monuments photos
    Signed-off-by: Strainu <github@strainu.ro>

--- robots/python/pwb/imagini/excommons.py
+++ robots/python/pwb/imagini/excommons.py
@@ -25,45 +25,58 @@ def save_fop_category():
     generator = pagegenerators.CategorizedPageGenerator(cat_page)
     for proposal_page in generator:
         for file_page in proposal_page.linkedPages(namespaces=6):
-            pywikibot.output(file_page.title())
-            if not file_page.exists():
-                pywikibot.output("File was already deleted, continue...")
-                continue
+            evacuate_file(file_page, 'ro', 'wikipedia')
 
-            rowiki = pywikibot.Site('ro','wikipedia')
+def save_relevant_templates():
+    opt = {
+        "templates_yes":"delete",
+        "templates_any":"Monument istoric\nSIRUTA\nCodRAN"
+    }
+    generator = pagegenerators.PetScanPageGenerator("", namespaces=[6], extra_options=opt)
+    for file_page in generator:
+        evacuate_file(pywikibot.FilePage(file_page), 'ro', 'wikipedia')
 
-            local_file_page = pywikibot.FilePage(rowiki, file_page.title())
-            if local_file_page.file_is_used == False:
-                pywikibot.output("File unused, continue...")
-                continue
-            if local_file_page.file_is_shared() == False:
-                if local_file_page.latest_file_info.sha1 == file_page.latest_file_info.sha1:
-                    pywikibot.output("File is already downloaded, continue...")
-                else:
-                    pywikibot.warn("File name conflict: name already exists on local wiki, but with different contents")
-                continue
+def evacuate_file(file_page, lang, family):
+    pywikibot.output(file_page.title())
+    if not file_page.exists():
+        pywikibot.output("File was already deleted, continue...")
+        return
 
-            local_text = file_page.get() + "\n{{FOP România}}"
-            builder = textlib.MultiTemplateMatchBuilder(rowiki)
-            template_regex = builder.pattern("delete")
-            local_text = re.sub(template_regex, "", local_text)
-            print(local_text)
+    local_site = pywikibot.Site(lang,family)
+
+    local_file_page = pywikibot.FilePage(local_site, file_page.title())
+    if local_file_page.file_is_used == False:
+        pywikibot.output("File unused, continue...")
+        return
+    if local_file_page.file_is_shared() == False:
+        if local_file_page.latest_file_info.sha1 == file_page.latest_file_info.sha1:
+            pywikibot.output("File is already downloaded, continue...")
+        else:
+            pywikibot.warn("File name conflict: name already exists on local wiki, but with different contents")
+        return
+
+    local_text = file_page.get() + "\n{{FOP România}}"
+    builder = textlib.MultiTemplateMatchBuilder(rowiki)
+    template_regex = builder.pattern("delete")
+    local_text = re.sub(template_regex, "", local_text)
+    print(local_text)
             
-            tmp_file_name = f"{os.getcwd()}/{file_page.title(as_filename=True, with_ns=False)}"
-            success = file_page.download(filename=tmp_file_name)
-            if success == False:
-                pywikibot.error(f"Error downloading {file_page.title()}")
-                os.remove(tmp_file_name)
-                return
-            success = local_file_page.upload(source=tmp_file_name, 
-                        comment=f"Evacuez un fișier ce va fi șters de la Commons", 
-                        text=local_text, 
-                        ignore_warnings=True)
-            os.remove(tmp_file_name)
-            if success:
-                pywikibot.output(f"Uploaded {local_file_page.title()}")
-            else:
-                pywikibot.error(f"Error uploading {local_file_page.title()}")
+    tmp_file_name = f"{os.getcwd()}/{file_page.title(as_filename=True, with_ns=False)}"
+    success = file_page.download(filename=tmp_file_name)
+    if success == False:
+        pywikibot.error(f"Error downloading {file_page.title()}")
+        os.remove(tmp_file_name)
+        return
+
+    success = local_file_page.upload(source=tmp_file_name, 
+               comment=f"Evacuez un fișier ce va fi șters de la Commons", 
+               text=local_text, 
+               ignore_warnings=True)
+    os.remove(tmp_file_name)
+    if success:
+        pywikibot.output(f"Uploaded {local_file_page.title()}")
+    else:
+        pywikibot.error(f"Error uploading {local_file_page.title()}")
 
 def main():
     op = None
@@ -75,6 +88,8 @@ def main():
 	
     if op == "fop":
         save_fop_category()
+    elif op == "fmt":
+        save_relevant_templates()
     else:
         pywikibot.error(f"Unknown operation {op}")
 
