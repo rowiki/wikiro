e6f006fe83ae5c470135f1c4537eb2dfa9d9a4a3
Strainu <github@strainu.ro>
2024-04-19 19:47:36 +0000

    monumente: Change RAN shortcuts per country
    Signed-off-by: Strainu <github@strainu.ro>

--- robots/python/pwb/monumente/ran_shortcuts.py
+++ robots/python/pwb/monumente/ran_shortcuts.py
@@ -14,48 +14,48 @@ See [[:ro:Wikipedia:Coduri]] (ro) for details on the Cod namespace.
 '''
 
 def main():
-	f = open("ran_db.json", "r+")
-	pywikibot.output("Reading database file...")
-	db = json.load(f)
-	pywikibot.output("...done")
-	f.close();
-	
-	#f = open("ro_pages.json", "r+")
-	#pywikibot.output("Reading articles file...")
-	#pages_ro = json.load(f)
-	#pywikibot.output("...done")
-	#f.close();
-	
-	site = pywikibot.getSite()
-	
-	#for code in pages_ro:
-	#	page = pywikibot.Page(site, u"Cod:RAN:" + code + "/articol")
-	#	#pywikibot.output(page.title())
-	#	if page.exists() and not page.isRedirectPage():
-	#			pywikibot.output(u"Page %s is not a redirect" % page.title())
-	#	else:
-	#	#if not page.exists():
-	#		page.put(u"#redirecteaza[[%s]]" % pages_ro[code][0]["name"], "Redirecting code to the Wikipedia article")
-			
-	for monument in db:
-		#if not monument["Cod"] in pages_ro:
-		#	continue
-		page = pywikibot.Page(site, u"Cod:RAN:" + monument["Cod"])
-		pywikibot.output(u"Cod:RAN:" + monument["Cod"])
-		if page.exists():
-			continue
+    f = open("ro_ran_db.json", "r+")
+    pywikibot.output("Reading database file...")
+    db = json.load(f)
+    pywikibot.output("...done")
+    f.close();
+    
+    #f = open("ro_pages.json", "r+")
+    #pywikibot.output("Reading articles file...")
+    #pages_ro = json.load(f)
+    #pywikibot.output("...done")
+    #f.close();
+    
+    site = pywikibot.Site()
+    
+    #for code in pages_ro:
+    #    page = pywikibot.Page(site, u"Cod:RAN:" + code + "/articol")
+    #    #pywikibot.output(page.title())
+    #    if page.exists() and not page.isRedirectPage():
+    #            pywikibot.output(u"Page %s is not a redirect" % page.title())
+    #    else:
+    #    #if not page.exists():
+    #        page.put(u"#redirecteaza[[%s]]" % pages_ro[code][0]["name"], "Redirecting code to the Wikipedia article")
+            
+    for monument in db:
+        #if not monument["Cod"] in pages_ro:
+        #    continue
+        page = pywikibot.Page(site, u"Cod:RAN:RO:" + monument["Cod"])
+        pywikibot.output(u"Cod:RAN:RO:" + monument["Cod"])
+        if page.exists():
+            continue
 
-		source_page = monument["source"][monument["source"].find(u'=')+1:monument["source"].find(u'&')]
-		pywikibot.output(source_page)
-		source_page = pywikibot.Page(site, source_page)
-		page_text = u"#redirect [[{0}#{1}]]".format(source_page.title(), monument["Cod"])
-		#print page_text
-		if not page.exists() or page.get(False, True) <> page_text:
-			page.put(page_text, "Redirecting code to the Wikipedia list")
+        source_page = monument["source"][monument["source"].find(u'=')+1:monument["source"].find(u'&')]
+        pywikibot.output(source_page)
+        source_page = pywikibot.Page(site, source_page)
+        page_text = u"#redirect [[{0}#{1}]]".format(source_page.title(), monument["Cod"])
+        #print page_text
+        if not page.exists() or page.get(False, True) != page_text:
+            page.put(page_text, "Redirecting code to the Wikipedia list")
 
 if __name__ == "__main__":
-	try:
-		main()
-	finally:
-		pywikibot.stopme()
+    try:
+        main()
+    finally:
+        pywikibot.stopme()
 
