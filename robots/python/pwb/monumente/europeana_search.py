058e665da1a3fdca649b4cf69932b724060e17a2
Strainu <github@strainu.ro>
2024-06-04 20:31:05 +0000

    monumente: Improve article creation
    Signed-off-by: Strainu <github@strainu.ro>

--- robots/python/pwb/monumente/europeana_search.py
+++ robots/python/pwb/monumente/europeana_search.py
@@ -5,10 +5,10 @@ Search for a text using the Europeana API, then check return all the entries as 
 
 Additionally, expose some functions that can be used to process the results
 '''
-import urllib
-import urllib2
 import json
 import re
+import requests
+import urllib.parse
 #'oYfxfaNeP'
 #'UTTpcWM7L'
 class EuropeanaInterface:
@@ -19,11 +19,11 @@ class EuropeanaInterface:
         
     def _getSlice(self, text, start, _type):
         url = "http://europeana.eu/api/v2/search.json?wskey={0}&query={1}&qf=TYPE:{2}&cursor={3}&rows={4}&profile=minimal&reusability=open"
-        actual_url = url.format(self._key, urllib.quote_plus(text), _type, urllib.quote_plus(self._cursor), self._rows)
-        print actual_url
-        response = urllib2.urlopen(actual_url)
-        txt = response.read()
-        return json.loads(txt)
+        actual_url = url.format(self._key, urllib.parse.quote_plus(text), _type, urllib.parse.quote_plus(self._cursor), self._rows)
+        print(actual_url)
+        response = requests.get(actual_url)
+        return response.json()
+        #return json.loads(txt)
         
     def searchEuropeana(self, text, filterCallback=None, filterField=None):
         next = 1
@@ -39,13 +39,15 @@ class EuropeanaInterface:
                 return []
             if "nextCursor" in _slice:
                 self._cursor = _slice["nextCursor"]
-                print self._cursor
+                print(self._cursor)
             else:
                 break
 
             #items.extend(_slice["items"])
             for elem in _slice["items"]:
-                if filterField not in elem or filterCallback == None or filterCallback(elem[filterField]):
+                if filterField is not None and (filterField not in elem or filterCallback is None or filterCallback(elem[filterField])):
+                    items.append(elem)
+                if filterField is None and (filterCallback is None or filterCallback(json.dumps(elem))):
                     items.append(elem)
             #print len(items)
             total = _slice["totalResults"]
@@ -58,14 +60,15 @@ class EuropeanaInterface:
         return items
     
     def getItem(self, url):
-        response = urllib2.urlopen(url)
-        txt = response.read()
-        return json.loads(txt)
+        response = requests.get(url)
+        return response.json()
+        #return json.loads(txt)
 
 def filterLMI(_id):
     #print _id
     return True
-    lmiregex = re.compile("(([a-z]{1,2})_(i|ii|iii|iv)_([a-z])_([a-z])_([0-9]{5}(\.[0-9]{2,3})?))", re.I)
+    #lmiregex = re.compile("(([a-z]{1,2})_(i|ii|iii|iv)_([a-z])_([a-z])_([0-9]{5}(\.[0-9]{2,3})?))", re.I)
+    lmiregex = re.compile("(([a-z]{1,2})([_-])(i|ii|iii|iv)([_-])([a-z])([_-])([a-z])([_-])([0-9]{5}([\._-][0-9]{2,3})?))", re.I)
     if re.search(lmiregex, _id):
         return True
     else:
@@ -77,18 +80,25 @@ if __name__ == "__main__":
     result = {}
     no = 0
     tot = 0
-    f = open("europeana_monuments.json", "w+")
+    search = "România"
+    field = None#"id"
+    filename = "europeana_search_" + search + ".json"
+    f = open("europeana_monuments.json", "wt")
     try:
-        f2 = open("europeana_search.json", "r")
-        search_results = json.load(f)
+        result = json.load(f)
+    except:
+        pass
+    try:
+        f2 = open(filename, "r")
+        search_results = json.load(f2)
     except Exception:
-        search_results = robot.searchEuropeana("România", filterLMI, "id")
-        f2 = open("europeana_search.json", "w+")
+        search_results = robot.searchEuropeana(search, filterLMI, field)
+        f2 = open(filename, "w+")
         json.dump(search_results, f2, indent=2)
     #for elem in robot.searchEuropeana("Institutul Național al Patrimoniului", filterLMI, "id"):
     for elem in search_results:
         tot += 1
-        print "Total: " + str(tot)
+        print("Total:", str(tot))
         if "link" not in elem:
             continue
 
@@ -103,15 +113,15 @@ if __name__ == "__main__":
         url = text["object"]["europeanaAggregation"]["edmLandingPage"]
 
         #print elem["id"]
-        lmi = re.search(lmiregex, elem["id"])
+        lmi = re.search(lmiregex, json.dumps(elem))
         if lmi:
             lmi = lmi.group(0).replace("_", "-")#TODO: the replace does not work for submonuments
         else:
             relations = []
             if "edmIsRelatedTo" in proxy:
-                relations = proxy["edmIsRelatedTo"]["def"]
-            print relations
-            for relation in relations:
+                relations = proxy["edmIsRelatedTo"].get("def")
+            print(relations)
+            for relation in relations or []:
                 lmi = re.search(lmiregex, relation)
                 if lmi:
                     lmi = lmi.group(0).replace("_", "-")
@@ -122,8 +132,14 @@ if __name__ == "__main__":
         else:
             lmi = re.sub(r'-([0-9]{2,3})$', '.\g<1>', lmi)
 
-        descr = ""
-        if "dcDescription" in proxy:
+        descr = None
+        if "dcDescriptionLangAware" in elem:
+            lst = elem["dcDescriptionLangAware"].get("ro") or [""]
+            for one_desc in lst:
+                if one_desc.find(lmi) > -1:
+                    descr = one_desc
+                    break
+        if descr is None and "dcDescription" in proxy:
             descr = proxy["dcDescription"]["ro"][0]
 
         if descr == None or \
@@ -145,8 +161,8 @@ if __name__ == "__main__":
                 'descr': descr
             }
         no += 1
-        print result[lmi]
-        print "Useful: " + str(no)
+        print(result[lmi])
+        print("Useful:", str(no))
         f.seek(0)
         json.dump(result, f, indent=2)
         
