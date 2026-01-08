bf99a1bb20c1f8440e9a94abdbcc60addd028fda
Andrei Stroe <andreistroe@users.noreply.github.com>
2026-01-08 12:27:19 +0200

    java: utils: fix WikipediaPageCache bugs

--- java/wikiro-java-utils/src/main/java/org/wikipedia/ro/utils/WikipediaPageCache.java
+++ java/wikiro-java-utils/src/main/java/org/wikipedia/ro/utils/WikipediaPageCache.java
@@ -15,27 +15,64 @@ public class WikipediaPageCache {
     private static final Logger LOG = Logger.getLogger(WikipediaPageCache.class.getCanonicalName());
     public static class CachedPage {
         private final String text;
-        private final boolean exists;
+        private final Boolean exist;
+        private final Boolean redirect;
 
-        public CachedPage(String text, boolean exists) {
+        public CachedPage(String text, Boolean exist, Boolean redirect) {
             this.text = text;
-            this.exists = exists;
+            this.exist = exist;
+            this.redirect = redirect;
         }
 
         public String getText() {
             return text;
         }
 
-        public boolean exists() {
-            return exists;
+        public Boolean exists() {
+            return exist;
+        }
+        
+        public Boolean redirects() {
+            return redirect;
         }
     }
 
     private final Map<String, CachedPage> cache = new ConcurrentHashMap<>();
 
+    public void loadPagesInfo(Wiki wiki, String... titles) {
+        List<String> titlesToLoad =
+            List.of(titles).stream().filter(title -> !cache.containsKey(computeCacheKey(wiki, title))).toList();
+        if (titlesToLoad.isEmpty()) {
+            return;
+        }
+        try {
+            List<Map<String, Object>> pageInfoMap = RetryHelper.retry(() -> {
+                try {
+                    return wiki.getPageInfo(titlesToLoad);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }, 3);
+
+            for (int titleIdx = 0; titleIdx < titlesToLoad.size(); titleIdx++) {
+                String cacheKey = computeCacheKey(wiki, titlesToLoad.get(titleIdx));
+                Map<String, Object> info = pageInfoMap.get(titleIdx);
+                Boolean exists = info != null && (Boolean) info.get("exists");
+                Boolean redirect = info != null && (Boolean) info.get("redirect");
+                cache.put(cacheKey, new CachedPage(cache.containsKey(cacheKey) ? cache.get(cacheKey).text : null, exists, redirect));
+            }
+        } catch (TimeoutException e) {
+            LOG.log(Level.SEVERE, e, () -> "Pages info failed to load for wiki " + wiki.getDomain());
+        }
+    }
+
+    public void loadPageTexts(Wiki wiki, String... titles) {
+        
+    }
+    
     public boolean pageExists(Wiki wiki, String title) {
         String cacheKey = computeCacheKey(wiki, title);
-        if (!cache.containsKey(title)) {
+        if (!cache.containsKey(cacheKey)) {
             boolean[] exists = null;
             try {
                 exists = RetryHelper.retry(() -> {
@@ -45,13 +82,13 @@ public class WikipediaPageCache {
                         throw new RuntimeException(e);
                     }
                 }, 3);
-                cache.put(cacheKey, new CachedPage(null, exists[0]));
+                cache.put(cacheKey, new CachedPage(null, exists[0], null));
             } catch (TimeoutException e) {
                 LOG.log(Level.SEVERE, e, () -> "Metainfo of page " + cacheKey + " failed to load");
                 return false;
             }
         }
-        return cache.get(cacheKey).exists; 
+        return cache.get(cacheKey).exist == Boolean.TRUE; 
     }
 
     private String computeCacheKey(Wiki wiki, String title) {
@@ -61,13 +98,14 @@ public class WikipediaPageCache {
     
     public CachedPage getPage(Wiki wiki, String title) {
         String cacheKey = computeCacheKey(wiki, title);
-        if (cache.containsKey(title) && cache.get(cacheKey).text != null) {
+        if (cache.containsKey(cacheKey) && cache.get(cacheKey).text != null) {
             return cache.get(cacheKey);
         }
         try {
-            boolean pageExists = pageExists(wiki, title);
+            loadPagesInfo(wiki, title);
             String pageText = null;
-            if (pageExists) {
+            CachedPage cachedPage = cache.get(cacheKey);
+            if (cachedPage.exist == Boolean.TRUE) {
                 List<String> texts = RetryHelper.retry(() -> {
                     try {
                         return wiki.getPageText(List.of(title));
@@ -77,12 +115,12 @@ public class WikipediaPageCache {
                 }, 3);
                 pageText = texts.get(0);
             }
-            cache.put(cacheKey, new CachedPage(pageText, pageExists));
+            cache.put(cacheKey, new CachedPage(pageText, cachedPage.exist, cachedPage.redirect));
         } catch (TimeoutException e) {
             LOG.log(Level.SEVERE, e, () -> "Page " + cacheKey + " failed to load");
         }
         
-        return cache.get(title);
+        return cache.get(cacheKey);
     }
     
     public String getPageText(Wiki wiki, String title) {
