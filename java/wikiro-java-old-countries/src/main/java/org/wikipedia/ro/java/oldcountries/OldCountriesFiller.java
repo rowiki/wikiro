4ad5cb83b97b5457f115b1379e7edb86383078bf
Andrei Stroe <andreistroe@users.noreply.github.com>
2026-09-21 19:10:47 +0300

    java: old-countries: J21 syntax

--- java/wikiro-java-old-countries/src/main/java/org/wikipedia/ro/java/oldcountries/OldCountriesFiller.java
+++ java/wikiro-java-old-countries/src/main/java/org/wikipedia/ro/java/oldcountries/OldCountriesFiller.java
@@ -106,19 +106,20 @@ public class OldCountriesFiller extends AbstractExecutable
     private WikidataEntitiesCache cache;
     private Map<String, UAT> countyIndex = new HashMap<>();
     //https://query.wikidata.org/#SELECT%20DISTINCT%20%3Fitem%20%3FitemLabel%20%3FcountyLabel%20WHERE%20%7B%0A%20%20VALUES%20%3Frouats%20%7Bwd%3AQ659103%20wd%3AQ16858213%20wd%3AQ640364%7D%0A%20%20%3Fitem%20wdt%3AP31%20%3Frouats.%0A%20%20%3Fitem%20wdt%3AP17%20wd%3AQ218.%0A%20%20%3Fitem%20wdt%3AP131%20%3Fcounty.%0A%20%20%3Fcounty%20wdt%3AP31%20wd%3AQ1776764.%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%0A%20%20%20%20bd%3AserviceParam%20wikibase%3Alanguage%20%22ro%2Cen%22.%0A%20%20%20%20%3Fitem%20rdfs%3Alabel%20%3FitemLabel.%0A%20%20%20%20%3Fcounty%20rdfs%3Alabel%20%3FcountyLabel.%0A%20%20%7D%0A%7D%0AORDER%20BY%20%3FcountyLabel%20%3FitemLabel
-    String allUATsQuery = "SELECT DISTINCT ?item ?county ?itemLabel ?countyLabel WHERE {\n"
-        + "  VALUES ?rouats {wd:Q659103 wd:Q16858213 wd:Q640364}\n"
-        + "  ?item wdt:P31 ?rouats.\n"
-        + "  ?item wdt:P17 wd:Q218.\n"
-        + "  ?item wdt:P131 ?county.\n"
-        + "  ?county wdt:P31 wd:Q1776764.\n"
-        + "  SERVICE wikibase:label {\n"
-        + "    bd:serviceParam wikibase:language \"ro,en\".\n"
-        + "    ?item rdfs:label ?itemLabel.\n"
-        + "    ?county rdfs:label ?countyLabel.\n"
-        + "  }\n"
-        + "}\n"
-        + "ORDER BY ?countyLabel ?itemLabel";
+    String allUATsQuery = """
+        SELECT DISTINCT ?item ?county ?itemLabel ?countyLabel WHERE {
+          VALUES ?rouats {wd:Q659103 wd:Q16858213 wd:Q640364}
+          ?item wdt:P31 ?rouats.
+          ?item wdt:P17 wd:Q218.
+          ?item wdt:P131 ?county.
+          ?county wdt:P31 wd:Q1776764.
+          SERVICE wikibase:label {
+            bd:serviceParam wikibase:language "ro,en".
+            ?item rdfs:label ?itemLabel.
+            ?county rdfs:label ?countyLabel.
+          }
+        }
+        ORDER BY ?countyLabel ?itemLabel""";
 
     @Override
     protected void execute() throws IOException, WikibaseException, LoginException
@@ -126,7 +127,7 @@ public class OldCountriesFiller extends AbstractExecutable
         
         List<Map<String, Object>> resultSet = dwiki.query(allUATsQuery);
         String startCounty = "Vâlcea";
-        String startCommune = "Amărăști";
+        String startCommune = "Berislăvești";
         String firstFoundCounty = null;
         
         dwiki.setAssertionMode(Wiki.ASSERT_USER | Wiki.ASSERT_BOT);
